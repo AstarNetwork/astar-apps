ef0c3ce1caa66d1ebdf005772c3fcbf15d42881f
Will Park <korea651@hanmail.net>
2021-08-31 21:08:10 +0900

    unlink to useaccount hook

--- src/hooks/useAccount.ts
+++ src/hooks/useAccount.ts
@@ -1,3 +1,4 @@
+//@deprecated
 import { reactive, toRefs, watch, watchEffect, computed, Ref } from 'vue';
 import { keyring } from '@polkadot/ui-keyring';
 import { useStore } from 'src/store';
@@ -19,53 +20,47 @@ export const useAccount = (currentAccount: Ref<string>, currentAccountName: Ref<
   const currentEcdsaAccount = computed(() => store.getters['general/currentEcdsaAccount']);
   const currentAccountIdx = computed(() => store.getters['general/accountIdx']);
 
-  // const state = reactive<UseAccounts>({
-    // allAccounts: [],
-    // allAccountNames: [],
-    // defaultAccount: '',
-    // defaultAccountName: '',
-    // defaultShortenAddress: '',
-    // hasAccounts: false,
-    // isAccount: () => false,
-  // });
+  const state = reactive<UseAccounts>({
+    allAccounts: [],
+    allAccountNames: [],
+    defaultAccount: '',
+    defaultAccountName: '',
+    defaultShortenAddress: '',
+    hasAccounts: false,
+    isAccount: () => false,
+  });
 
-  // const accounts = keyring.getAccounts();
-  // console.log('accounts', accounts);
-  // accounts.forEach(({ address, meta, publicKey }) =>
-  //   console.log(address, JSON.stringify(meta), u8aToHex(publicKey))
-  // );
+  watchEffect((onInvalidate) => {
+    const subscription = keyring.accounts.subject.subscribe((accounts) => {
+      state.allAccounts = accounts ? Object.keys(accounts) : [];
+      state.allAccountNames = accounts
+        ? Object.values(accounts).map((obj) => obj.option.name)
+        : [];
+      state.defaultAccount =
+        state.allAccounts.length > 0 ? Object.keys(accounts)[0] : '';
+      state.defaultAccountName =
+        state.allAccounts.length > 0
+          ? Object.values(accounts)[0].option.name
+          : '';
+      state.defaultShortenAddress = state.defaultAccount.length > 0
+        ? `${state.defaultAccount.slice(0, 6)}${'.'.repeat(6)}${state.defaultAccount.slice(-6)}`
+        : '';
+      state.hasAccounts = state.allAccounts.length !== 0;
+      state.isAccount = (address: string) =>
+        state.allAccounts.includes(address);
+    });
 
-  // watchEffect((onInvalidate) => {
-  //   const subscription = keyring.accounts.subject.subscribe((accounts) => {
-  //     state.allAccounts = accounts ? Object.keys(accounts) : [];
-  //     state.allAccountNames = accounts
-  //       ? Object.values(accounts).map((obj) => obj.option.name)
-  //       : [];
-      // state.defaultAccount =
-      //   state.allAccounts.length > 0 ? Object.keys(accounts)[0] : '';
-      // state.defaultAccountName =
-      //   state.allAccounts.length > 0
-      //     ? Object.values(accounts)[0].option.name
-      //     : '';
-      // state.defaultShortenAddress = state.defaultAccount.length > 0
-      //   ? `${state.defaultAccount.slice(0, 6)}${'.'.repeat(6)}${state.defaultAccount.slice(-6)}`
-      //   : '';
-      // state.hasAccounts = state.allAccounts.length !== 0;
-      // state.isAccount = (address: string) =>
-      //   state.allAccounts.includes(address);
-    // });
-
-  //   onInvalidate(() => {
-  //     subscription.unsubscribe();
-  //   });
-  // });
+    onInvalidate(() => {
+      subscription.unsubscribe();
+    });
+  });
 
   watch(
     currentAccountIdx,
     () => {
-      // currentAccount.value = state.allAccounts[currentAccountIdx.value];
-      // currentAccountName.value =
-      //   state.allAccountNames[currentAccountIdx.value];
+      currentAccount.value = state.allAccounts[currentAccountIdx.value];
+      currentAccountName.value =
+        state.allAccountNames[currentAccountIdx.value];
     },
     { immediate: true }
   );
@@ -77,13 +72,13 @@ export const useAccount = (currentAccount: Ref<string>, currentAccountName: Ref<
         currentAccount.value = currentEcdsaAccount.value.ss58;
         currentAccountName.value = 'ECDSA (Ethereum Extension)';
       } else {
-        // currentAccount.value = state.allAccounts[currentAccountIdx.value];
-        // currentAccountName.value =
-        //   state.allAccountNames[currentAccountIdx.value];
+        currentAccount.value = state.allAccounts[currentAccountIdx.value];
+        currentAccountName.value =
+          state.allAccountNames[currentAccountIdx.value];
       }
     },
     { immediate: true }
   )
 
-  // return toRefs(state);
+  return toRefs(state);
 };
