bc68bdaa03e6207154c9b770e52efc515aa361a0
Hoon Kim <40356749+hoonsubin@users.noreply.github.com>
2021-11-30 19:03:03 +0100

    fix div tag

--- src/components/store/StakePanel.vue
+++ src/components/store/StakePanel.vue
@@ -27,9 +27,10 @@
             </div>
           </div>
 
-        <div :style="{ opacity: stakeInfo?.hasStake ? '1' : '0' }" class="tw-flex tw-flex-row">
-          <div class="tw-w-20">{{ $t('store.yourStake') }}</div>
-          <div class="tw-font-semibold">{{ stakeInfo?.yourStake.formatted }}</div>
+          <div :style="{ opacity: stakeInfo?.hasStake ? '1' : '0' }" class="tw-flex tw-flex-row">
+            <div class="tw-w-20">{{ $t('store.yourStake') }}</div>
+            <div class="tw-font-semibold">{{ stakeInfo?.yourStake.formatted }}</div>
+          </div>
         </div>
       </div>
       <div class="tw-flex">
