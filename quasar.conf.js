0b84516f197937abbb1b61b6da5f4ce7973f730c
Will Park <korea651@hanmail.net>
2022-01-19 20:17:36 +0900

    enable webpack-bundle-analyzer

--- quasar.conf.js
+++ quasar.conf.js
@@ -70,7 +70,7 @@ module.exports = configure(function (ctx) {
       // preloadChunks: true,
       // showProgress: false,
       // gzip: true,
-      // analyze: true,
+      analyze: true,
 
       // Options below are automatically set depending on the env, set them if you want to override
       // extractCSS: false,
