72c2f0f566e1a0da1fd083a832a197c4320a59ab
Will Park <korea651@hanmail.net>
2022-01-24 16:36:19 +0900

    remove duplicate font usage

--- quasar.conf.js
+++ quasar.conf.js
@@ -51,7 +51,7 @@ module.exports = configure(function (ctx) {
       // 'line-awesome',
       // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
 
-      'roboto-font', // optional, you are not bound to it
+      // 'roboto-font', // optional, you are not bound to it
       'material-icons', // optional, you are not bound to it
     ],
 
