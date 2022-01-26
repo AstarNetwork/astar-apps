b4590127aabcb45bb7d41dad11bc2cd057fad8e1
Will Park <korea651@hanmail.net>
2022-01-26 17:32:08 +0900

    remove axios boot file

--- quasar.conf.js
+++ quasar.conf.js
@@ -32,7 +32,6 @@ module.exports = configure(function (ctx) {
     // https://v2.quasar.dev/quasar-cli/boot-files
     boot: [
       'i18n',
-      'axios',
     ],
 
     // https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
