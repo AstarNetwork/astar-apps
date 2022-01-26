b9e8a74a35fc70db60004e1a2ee3af55098789cf
Will Park <korea651@hanmail.net>
2022-01-26 18:07:09 +0900

    set analyze as false

--- quasar.conf.js
+++ quasar.conf.js
@@ -69,7 +69,7 @@ module.exports = configure(function (ctx) {
       // preloadChunks: true,
       // showProgress: false,
       // gzip: true,
-      analyze: true,
+      analyze: false,
 
       // Options below are automatically set depending on the env, set them if you want to override
       // extractCSS: false,
