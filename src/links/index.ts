6621904f395aa79fd988463412f7f43b1bd158b2
impelcrypto <impelcrypto@gmail.com>
2022-02-11 12:59:14 +0800

    fix: import social urls

--- src/links/index.ts
+++ src/links/index.ts
@@ -3,3 +3,10 @@ export const docsUrl = {
   evmDeposit:
     'https://docs.astar.network/tutorial/how-to-send-astr-sdn-from-metamask-to-polkadot.js',
 };
+
+export const socialUrl = {
+  twitter: 'https://twitter.com/AstarNetwork',
+  telegram: 'https://t.me/PlasmOfficial',
+  discord: 'https://discord.gg/Z3nC9U4',
+  github: 'https://github.com/AstarNetwork',
+};
