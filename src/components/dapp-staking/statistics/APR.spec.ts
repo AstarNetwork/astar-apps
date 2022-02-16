7cc9877c8696d0573a4e7490cda05f3361003172
Bobo <bobo.kovacevic@gmail.com>
2022-02-16 10:42:19 +0100

    Uncommented problematic test

--- src/components/dapp-staking/statistics/APR.spec.ts
+++ src/components/dapp-staking/statistics/APR.spec.ts
@@ -1,26 +1,26 @@
 // TODO work on this will be continued in another branch
 
-// import { shallowMount } from '@vue/test-utils';
-// import { QIcon } from 'quasar';
-// import * as apr from 'src/hooks/useApr';
-// import APR from 'src/components/dapp-staking/statistics/APR.vue';
+import { shallowMount } from '@vue/test-utils';
+import { QIcon } from 'quasar';
+import * as apr from 'src/hooks/useApr';
+import APR from 'src/components/dapp-staking/statistics/APR.vue';
 
-// const mock = jest.spyOn(apr, 'useApr');
-// const wrapper = shallowMount(APR, {
-//   global: {
-//     components: {
-//       QIcon,
-//     },
-//     mocks: {
-//       $t: (msg: string) => msg,
-//     },
-//   },
-// });
+const mock = jest.spyOn(apr, 'useApr');
+const wrapper = shallowMount(APR, {
+  global: {
+    components: {
+      QIcon,
+    },
+    mocks: {
+      $t: (msg: string) => msg,
+    },
+  },
+});
 
-// describe('APR', () => {
-//   it('calls useApr', () => {
-//     console.log('in test');
-//     expect(mock).toBeCalled();
-//     mock.mockRestore();
-//   });
-// });
+describe('APR', () => {
+  it('calls useApr', () => {
+    console.log('in test');
+    expect(mock).toBeCalled();
+    mock.mockRestore();
+  });
+});
