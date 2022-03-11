0f65eb2800907b74cd8b7e48c8f24c9cf179bb29
impelcrypto <impelcrypto@gmail.com>
2022-03-11 12:41:04 +0800

    config: css variables

---
+++ src/components/assets/Assets.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>hello</div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, toRefs } from 'vue';
+import { useChainMetadata } from 'src/hooks';
+
+// import Balance from 'components/common/Balance.vue';
+
+export default defineComponent({
+  // components: { Balance },
+
+  setup(props) {
+    // console.log('unit', defaultUnitToken.value);
+    return {};
+  },
+  methods: {},
+});
+</script>
--- src/pages/Assets.vue
+++ src/pages/Assets.vue
@@ -1,36 +1,34 @@
 <template>
   <div>
-    <div>
+    <div class="size-lg">
       <span class="title">
         {{ $t('assets.assets') }}
       </span>
     </div>
-    <!-- <router-view /> -->
+    <Assets />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { useMeta } from 'quasar';
+import Assets from 'src/components/assets/Assets.vue';
 
 export default defineComponent({
-  components: {},
+  components: { Assets },
   setup() {
     useMeta({ title: 'Assets' });
   },
 });
 </script>
-<style>
+
+<style lang="scss" scoped>
+@import 'src/components/assets/styles/assets.scss';
+
 .dark\:tw-bg-blue-500 {
   background-color: rgba(27, 109, 193, 0.1) !important;
 }
 .checked\:tw-border-blue-500:checked {
   border-color: rgba(27, 109, 193, 1) !important;
-}
-
-.title {
-  font-family: 'SF Pro';
-  font-weight: 600;
-  font-size: 26px;
 }
 </style>
