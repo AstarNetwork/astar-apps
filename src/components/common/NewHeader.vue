1a09752c1c9569bde0e661bac48cbb3d13717578
Will <korea651@hanmail.net>
2022-03-18 11:47:46 +0900

    add header comp

--- src/components/common/NewHeader.vue
+++ src/components/common/NewHeader.vue
@@ -1,22 +1,18 @@
 <template>
-  <div>
-    <Button>Test</Button>
+  <astar-header title="Test">
     <my-button>Test2</my-button>
     <div v-for="(n, i) in 7" :key="i">
       <astar-text :type="`H${i + 1}`">H {{ i + 1 }}</astar-text>
     </div>
-    <Foo />
-  </div>
+  </astar-header>
 </template>
 
 <script lang="ts">
 import { useSidebar } from 'src/hooks';
 import { defineComponent } from 'vue';
 import { useStore } from 'src/store';
-import { Button, Foo } from 'astar-ui';
 
 export default defineComponent({
-  components: { Button, Foo },
   setup() {
     const { isOpen } = useSidebar();
     const store = useStore();
