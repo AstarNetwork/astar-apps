12b5dc1c594e4fd44f02f76078b3a38766b4048f
impelcrypto <impelcrypto@gmail.com>
2022-04-05 12:52:38 +0800

    test: modal size for small devices

--- quasar.conf.js
+++ quasar.conf.js
@@ -90,7 +90,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
-      https: false,
+      https: true,
       port: 8080,
       open: true, // opens browser window automatically
     },
