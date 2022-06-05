fe71ca85e61a798e3b9003fc81c50c52ca645651
Pedro Filho <pedro@filho.me>
2022-06-05 18:04:19 -0300

    adding option to set talismanEth

--- src/hooks/custom-signature/useEthProvider.ts
+++ src/hooks/custom-signature/useEthProvider.ts
@@ -1,14 +1,31 @@
-import { ref, watchEffect } from 'vue';
+import { SupportWallet } from 'src/config/wallets';
+import { useStore } from 'src/store';
+import { ref, watchEffect, computed } from 'vue';
 import { EthereumProvider } from '../types/CustomSignature';
 
 export function useEthProvider() {
   const ethProvider = ref<EthereumProvider>();
 
+  const store = useStore();
+  const currentWallet = computed(() => store.getters['general/currentWallet']);
+
   // TODO find a way to se if is talisman or metamask
 
   watchEffect(() => {
-    if (typeof window.ethereum !== 'undefined' && window.ethereum) {
+    if (
+      typeof window.ethereum !== 'undefined' &&
+      window.ethereum &&
+      currentWallet.value === SupportWallet.MetaMask
+    ) {
       ethProvider.value = window.ethereum;
+    }
+
+    if (
+      typeof window.talismanEth !== 'undefined' &&
+      window.talismanEth &&
+      currentWallet.value === SupportWallet.Talisman
+    ) {
+      ethProvider.value = window.talismanEth;
     }
   });
 
