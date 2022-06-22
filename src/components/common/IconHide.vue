2bcc81dfebb1e7fd1aa5300513233559e4832863
impelcrypto <impelcrypto@gmail.com>
2022-06-22 15:07:16 +0800

    feat: import icons from Astar-UI

--- src/components/common/IconHide.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <span class="material-symbols-outlined"> visibility_off </span>
-</template>
-
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({});
-</script>
