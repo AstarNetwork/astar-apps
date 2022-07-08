36b8906d3e3e792dd9f073c8df20a4246dc12a96
impelcrypto <impelcrypto@gmail.com>
2022-07-08 20:13:03 +0800

    fix: app uses the 'token-api' to fetch the usd price

--- src/hooks/helper/price.ts
+++ src/hooks/helper/price.ts
@@ -1,61 +1,14 @@
-import { wrappedTokenMap } from './../../modules/token/index';
+import { TOKEN_API_URL } from 'src/modules/token-api';
 import axios from 'axios';
 
-const getUsdPriceViaDia = async (symbol: string): Promise<number> => {
-  try {
-    // Ref: https://docs.diadata.org/documentation/api-1/api-endpoints#quotation
-    const url = `https://api.diadata.org/v1/quotation/${symbol.toUpperCase()}`;
-    const result = await axios.get(url);
-    const price = result.data.Price;
-    return price;
-  } catch (error) {
-    return getUsdBySymbolViaCoingecko(symbol);
-  }
-};
-
-export const getUsdBySymbolViaCoingecko = async (symbol: string): Promise<number> => {
-  try {
-    const url = 'https://api.coingecko.com/api/v3/coins/list';
-    const { data } = await axios.get<{ id: string; symbol: string }[]>(url);
-    const item = data.find((it) => it.symbol.toLowerCase() === symbol.toLowerCase());
-    if (!item) return 0;
-    return await getUsdPrice(item.id);
-  } catch (error) {
-    console.error(error);
-    console.error(`symbol: ${symbol}`);
-    return 0;
-  }
-};
-
 export const getUsdBySymbol = async (symbol: string): Promise<number> => {
-  let tokenSymbol = symbol;
-  if (wrappedTokenMap.hasOwnProperty(symbol)) {
-    tokenSymbol = wrappedTokenMap[symbol as keyof typeof wrappedTokenMap];
-  }
   try {
-    return getUsdPriceViaDia(tokenSymbol);
+    const url = TOKEN_API_URL + '/v1/token/price/' + symbol;
+    const { data } = await axios.get<number>(url);
+    return data;
   } catch (error) {
     console.error(error);
-    try {
-      return getUsdBySymbolViaCoingecko(tokenSymbol);
-    } catch (error) {
-      console.error(error);
-      console.error(`symbol: ${symbol}`);
-      return 0;
-    }
-  }
-};
-
-export const getUsdPrice = async (currency: string): Promise<number> => {
-  try {
-    const url = `https://api.coingecko.com/api/v3/simple/price?ids=${currency}&vs_currencies=usd`;
-    const result = await axios.get(url);
-    const price = result.data[currency].usd;
-    return Number(price);
-  } catch (error) {
-    console.error(error);
-    console.error(`currency: ${currency}`);
-    return getUsdPriceViaDia(currency);
+    return 0;
   }
 };
 
