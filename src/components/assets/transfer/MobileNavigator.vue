ec55c7cd1f5d050958d92ec115700f0e4bc544b4
impelcrypto <impelcrypto@gmail.com>
2022-08-03 16:25:04 +0800

    feat: added MobileNavigator

--- src/components/assets/transfer/MobileNavigator.vue
+++ src/components/assets/transfer/MobileNavigator.vue
@@ -1,14 +1,34 @@
 <template>
   <div class="container--mobile-navigator">
-    <div>FAQ</div>
-    <div>Recent History</div>
-    <div>Hot Topic</div>
+    <button class="circle--button">
+      <astar-icon-group :size="size" />
+      <span>FAQ</span>
+    </button>
+    <button class="circle--button">
+      <astar-icon-history :size="size" />
+      <span>Recent History</span>
+    </button>
+    <button class="circle--button">
+      <astar-icon-group :size="size" />
+      <span>Hot Topic</span>
+    </button>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { useBreakpoints } from 'src/hooks';
+import { defineComponent, computed } from 'vue';
 
-export default defineComponent({});
+export default defineComponent({
+  setup() {
+    const { screenSize, width } = useBreakpoints();
+    const size = computed(() => (width.value > screenSize.sm ? '24' : '21'));
+
+    return {
+      screenSize,
+      size,
+    };
+  },
+});
 </script>
 
 <style lang="scss" scoped>
