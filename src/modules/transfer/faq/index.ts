b3db3629ed804d35128dac8b24e5c4fd121a93a5
impelcrypto <impelcrypto@gmail.com>
2022-08-25 17:05:08 +0800

    docs: updated FAQ articles

--- src/modules/transfer/faq/index.ts
+++ src/modules/transfer/faq/index.ts
@@ -2,15 +2,15 @@ import { Faq } from '../index';
 
 export const faqH160Transfer: Faq[] = [
   {
-    title: 'Creating a Astar Native account',
+    title: 'What is Astar Native Account and how do I create it?',
     url: 'https://docs.astar.network/docs/user-guides/create-wallet',
   },
   {
-    title: 'Sending ASTR/SDN from Exchanges to our portal',
+    title: 'How do I send ASTR to Exchanges?',
     url: 'https://docs.astar.network/docs/user-guides/exchange-to-portal',
   },
   {
-    title: 'Swap your assets in other chains to ASTR',
+    title: 'What is the easiest way to bring tokens to Astar from other chains?',
     url: 'https://docs.astar.network/docs/user-guides/xc-swap',
   },
   {
@@ -21,12 +21,16 @@ export const faqH160Transfer: Faq[] = [
 
 export const faqH160XcmBridge: Faq[] = [
   {
-    title: 'Swap your assets in other chains to ASTR',
+    title: 'How can I find my other chains addresses?',
+    url: 'https://docs.astar.network/docs/xcm/faq#q-where-can-i-find-other-chains-addresses',
+  },
+  {
+    title: 'What is the easiest way to bring tokens to Astar from other chains?',
     url: 'https://docs.astar.network/docs/user-guides/xc-swap',
   },
   {
     title: 'What is ED (Existential Deposit)?',
-    url: 'https://support.polkadot.network/support/solutions/articles/65000168651-what-is-the-existential-deposit-',
+    url: 'https://docs.astar.network/docs/user-guides/xc-swap',
   },
   {
     title: 'Any video tutorials available for new users?',
@@ -36,11 +40,11 @@ export const faqH160XcmBridge: Faq[] = [
 
 export const faqSs58Transfer: Faq[] = [
   {
-    title: 'How to send ASTR/SDN from Exchanges to our portal',
+    title: 'How can I send ASTR/SDN from Exchanges to Portal?',
     url: 'https://docs.astar.network/docs/user-guides/exchange-to-portal',
   },
   {
-    title: 'Swap your assets in other chains to ASTR',
+    title: 'What is ED (Existential Deposit)?',
     url: 'https://docs.astar.network/docs/user-guides/xc-swap',
   },
   {
@@ -55,7 +59,11 @@ export const faqSs58XcmBridge: Faq[] = [
     url: 'https://docs.astar.network/docs/xcm/',
   },
   {
-    title: 'Swap your assets in other chains to ASTR',
+    title: 'How can I find my other chains addresses?',
+    url: 'https://docs.astar.network/docs/xcm/faq#q-where-can-i-find-other-chains-addresses',
+  },
+  {
+    title: 'What is the easiest way to bring tokens to Astar from other chains?',
     url: 'https://docs.astar.network/docs/user-guides/xc-swap',
   },
   {
