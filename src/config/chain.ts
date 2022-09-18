c6867f19f9a368a220c02434be75550d7150407f
impelcrypto <impelcrypto@gmail.com>
2022-09-18 20:08:58 +0800

    refactor: clean up

--- src/config/chain.ts
+++ src/config/chain.ts
@@ -9,10 +9,9 @@ export enum astarChain {
 
 export type ASTAR_CHAIN = astarChain.SHIDEN | astarChain.ASTAR | astarChain.SHIBUYA;
 
-export type ASTAR_NATIVE_TOKEN = 'ASTR' | 'SDN' | 'SBY';
-
 export type ASTAR_NETWORK_IDX = endpointKey.ASTAR | endpointKey.SHIDEN | endpointKey.SHIBUYA;
 
 export type ASTAR_EVM_NETWORK_IDX = 592 | 336 | 81;
 
+export type ASTAR_NATIVE_TOKEN = 'ASTR' | 'SDN' | 'SBY';
 export const astarMainnetNativeToken: ASTAR_NATIVE_TOKEN[] = ['ASTR', 'SDN'];
