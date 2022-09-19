57a1cb56e3cf27864b7e1c7588bb5874590f0de3
impelcrypto <impelcrypto@gmail.com>
2022-09-19 11:14:37 +0800

    fix: avoid not rendering anything when users go to other pages

--- src/pages/AssetsPage.vue
+++ src/pages/AssetsPage.vue
@@ -1,15 +1,24 @@
 <template>
-  <!-- Fixme: scroll to top when the app was routed from other page -->
-  <router-view id="assets-top" />
+  <div v-if="currentAccount">
+    <!-- Fixme: scroll to top when the app was routed from other page -->
+    <router-view id="assets-top" />
+  </div>
+  <div v-else />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
 import { useMeta } from 'quasar';
+import { useAccount } from 'src/hooks';
+import { defineComponent } from 'vue';
+
+// <div v-else /> Memo: To avoid not rendering anything when users go to other pages
+// Leaving comments on the `template` caused an unknown bug
 
 export default defineComponent({
   setup() {
     useMeta({ title: 'Assets' });
+    const { currentAccount } = useAccount();
+    return { currentAccount };
   },
   // mounted() {
   // Memo: scrollBehavior in createRouter is not working
