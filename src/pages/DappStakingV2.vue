4b8ab36a6f8fdf58ab1a703ed3ede5add0ae951b
Will <korea651@hanmail.net>
2022-09-23 22:07:58 +0900

    fix conflicts

--- src/pages/DappStakingV2.vue
+++ src/pages/DappStakingV2.vue
@@ -7,25 +7,9 @@
 </template>
 
 <script lang="ts">
-import { formatUnitAmount } from 'src/hooks/helper/plasmUtils';
-import { useStore } from 'src/store';
-import { computed, defineComponent } from 'vue';
+import { defineComponent } from 'vue';
 export default defineComponent({
-  setup() {
-    const store = useStore();
-    const maxNumberOfStakersPerContract = computed(
-      () => store.getters['dapps/getMaxNumberOfStakersPerContract']
-    );
-    const minimumStakingAmount = computed(() => {
-      const amount = store.getters['dapps/getMinimumStakingAmount'];
-      return formatUnitAmount(amount);
-    });
-
-    return {
-      maxNumberOfStakersPerContract,
-      minimumStakingAmount,
-    };
-  },
+  setup() {},
 });
 </script>
 
