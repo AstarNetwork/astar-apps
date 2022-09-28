035c8faa44257ca8587fabbacc20a63fef66cb12
impelcrypto <impelcrypto@gmail.com>
2022-09-28 13:54:50 +0800

    refactor: import Chain enum for 'originChain' property instead of hard-coding the value

--- src/modules/xcm/tokens/index.ts
+++ src/modules/xcm/tokens/index.ts
@@ -1,5 +1,5 @@
 import { Erc20Token } from 'src/modules/token';
-import { Asset } from 'src/v2/models';
+import { Asset, Chain } from 'src/v2/models';
 import { XcmTokenInformation } from 'src/modules/xcm';
 import { endpointKey } from 'src/config/chainEndpoints';
 import { ASTAR_DECIMALS } from 'src/hooks/helper/plasmUtils';
@@ -21,7 +21,7 @@ export const xcmToken = {
       originAssetId: '',
       logo: require('/src/assets/img/ic_dot.png'),
       isXcmCompatible: true,
-      originChain: 'Polkadot',
+      originChain: Chain.POLKADOT,
       minBridgeAmount: '1.1',
     },
     {
@@ -31,7 +31,7 @@ export const xcmToken = {
       originAssetId: 'ACA',
       logo: 'https://assets.coingecko.com/coins/images/20634/small/upOKBONH_400x400.jpg?1647420536',
       isXcmCompatible: true,
-      originChain: 'Acala',
+      originChain: Chain.ACALA,
       minBridgeAmount: '0.4',
     },
     {
@@ -41,7 +41,7 @@ export const xcmToken = {
       originAssetId: 'LDOT',
       logo: 'https://assets.coingecko.com/coins/images/25847/small/iShot2022-06-02_13.14.07-removebg-preview.png?1654146888',
       isXcmCompatible: true,
-      originChain: 'Acala',
+      originChain: Chain.ACALA,
       minBridgeAmount: '0.13',
     },
     {
@@ -51,7 +51,7 @@ export const xcmToken = {
       originAssetId: 'AUSD',
       logo: 'https://assets.coingecko.com/coins/images/25812/small/ezgif-1-f4612f5260.png?1653987299',
       isXcmCompatible: true,
-      originChain: 'Acala',
+      originChain: Chain.ACALA,
       minBridgeAmount: '0.1',
     },
     {
@@ -61,7 +61,7 @@ export const xcmToken = {
       originAssetId: 'GLMR',
       logo: 'https://assets.coingecko.com/coins/images/22459/small/glmr.png?1641880985',
       isXcmCompatible: true,
-      originChain: 'Moonbeam',
+      originChain: Chain.MOONBEAM,
       minBridgeAmount: '0.2',
     },
     {
@@ -71,7 +71,7 @@ export const xcmToken = {
       originAssetId: '1984',
       logo: 'https://assets.coingecko.com/coins/images/325/small/Tether-logo.png?1598003707',
       isXcmCompatible: true,
-      originChain: 'Statemint',
+      originChain: Chain.STATEMINT,
       minBridgeAmount: '0.21',
     },
     {
@@ -81,7 +81,7 @@ export const xcmToken = {
       originAssetId: 'IBTC',
       logo: 'https://assets.coingecko.com/coins/images/26217/small/interBTC_for_Exchanges.png?1656578114',
       isXcmCompatible: true,
-      originChain: 'Interlay',
+      originChain: Chain.INTERLAY,
       minBridgeAmount: '0.00000237',
     },
     {
@@ -91,7 +91,7 @@ export const xcmToken = {
       originAssetId: 'INTR',
       logo: 'https://assets.coingecko.com/coins/images/26180/small/Interlay-Coinbase-2.png?1656382486',
       isXcmCompatible: true,
-      originChain: 'Interlay',
+      originChain: Chain.INTERLAY,
       minBridgeAmount: '0.959',
     },
   ],
@@ -103,7 +103,7 @@ export const xcmToken = {
       originAssetId: '',
       logo: require('/src/assets/img/ic_kusama.png'),
       isXcmCompatible: true,
-      originChain: 'Kusama',
+      originChain: Chain.KUSAMA,
       minBridgeAmount: '0.1',
     },
     {
@@ -113,7 +113,7 @@ export const xcmToken = {
       originAssetId: 'KUSD',
       logo: 'https://assets.coingecko.com/coins/images/25812/small/ezgif-1-f4612f5260.png?1653987299',
       isXcmCompatible: true,
-      originChain: 'Karura',
+      originChain: Chain.KARURA,
       minBridgeAmount: '0.1',
     },
     {
@@ -123,7 +123,7 @@ export const xcmToken = {
       originAssetId: 'KAR',
       logo: 'https://assets.coingecko.com/coins/images/17172/small/karura.jpeg?1626782066',
       isXcmCompatible: true,
-      originChain: 'Karura',
+      originChain: Chain.KARURA,
       // ED: 0.1 KAR (Users can't withdraw 0.1KAR to 0 KAR account due to a fee)
       minBridgeAmount: '0.11',
     },
@@ -134,7 +134,7 @@ export const xcmToken = {
       originAssetId: 'LKSM',
       logo: 'https://resources.acala.network/tokens/LKSM.png',
       isXcmCompatible: true,
-      originChain: 'Karura',
+      originChain: Chain.KARURA,
       minBridgeAmount: '0.0024',
     },
     {
@@ -144,7 +144,7 @@ export const xcmToken = {
       originAssetId: 'MOVR',
       logo: 'https://assets.coingecko.com/coins/images/17984/small/9285.png?1630028620',
       isXcmCompatible: true,
-      originChain: 'Moonriver',
+      originChain: Chain.MOONRIVER,
       minBridgeAmount: '0.007',
     },
     {
@@ -154,7 +154,7 @@ export const xcmToken = {
       originAssetId: '1984',
       logo: 'https://assets.coingecko.com/coins/images/325/small/Tether-logo.png?1598003707',
       isXcmCompatible: true,
-      originChain: 'Statemine',
+      originChain: Chain.STATEMINE,
       minBridgeAmount: '0.1',
     },
     {
@@ -164,7 +164,7 @@ export const xcmToken = {
       originAssetId: 'KBTC',
       logo: 'https://assets.coingecko.com/coins/images/25816/small/jKEvMy-9_400x400.jpeg?1653990781',
       isXcmCompatible: true,
-      originChain: 'Kintsugi',
+      originChain: Chain.KINTSUGI,
       minBridgeAmount: '0.00000237',
     },
     {
@@ -174,7 +174,7 @@ export const xcmToken = {
       originAssetId: 'KINT',
       logo: 'https://assets.coingecko.com/coins/images/22045/small/Kintsugi_logo-150x150.jpeg?1640675060',
       isXcmCompatible: true,
-      originChain: 'Kintsugi',
+      originChain: Chain.KINTSUGI,
       minBridgeAmount: '0.345',
     },
   ],
@@ -294,7 +294,7 @@ export const SDN: XcmTokenInformation = {
   originAssetId: 'SDN',
   logo: require('src/assets/img/sdn-token.png'),
   isXcmCompatible: true,
-  originChain: 'Shiden',
+  originChain: Chain.SHIDEN,
   minBridgeAmount: '0.1',
 };
 
@@ -305,7 +305,7 @@ export const ASTR: XcmTokenInformation = {
   originAssetId: 'ASTR',
   logo: require('src/assets/img/astr-token.png'),
   isXcmCompatible: true,
-  originChain: 'Astar',
+  originChain: Chain.ASTAR,
   minBridgeAmount: '0.1',
 };
 
