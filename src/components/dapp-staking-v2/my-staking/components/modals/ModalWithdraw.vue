17e4e3e04069cc3c8965284fd8ac34f091293064
Will <korea651@hanmail.net>
2022-10-18 00:03:55 +0900

    adapt withdraw modal for test

---
+++ src/components/dapp-staking-v2/my-staking/components/modals/ModalWithdraw.vue
@@ -0,0 +1,40 @@
+<template>
+  <astar-default-modal :width="544" :height="381" :show="show" title="Withdraw">
+    <div>Your funds are available to withdraw.</div>
+    <div class="box container">
+      <div>
+        <span class="text--md">Ready to withdraw</span>
+      </div>
+      <div class="text--md">10,000 ASTR</div>
+    </div>
+    <astar-button width="320" height="42">Confirm</astar-button>
+  </astar-default-modal>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    show: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup() {},
+});
+</script>
+
+<style lang="scss" scoped>
+.container {
+  margin-top: 20px;
+
+  div {
+    margin: 10px 0px;
+  }
+
+  a {
+    color: $astar-blue;
+  }
+}
+</style>
