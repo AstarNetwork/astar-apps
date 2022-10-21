9eb388ed52c9728115061b670b4b6aec56724891
impelcrypto <impelcrypto@gmail.com>
2022-10-21 17:37:58 +0900

    fix: scroll to top whenever the app link to dApp page

--- src/pages/DappPage.vue
+++ src/pages/DappPage.vue
@@ -1,16 +1,28 @@
 <template>
-  <dapp class="dapp-top" />
+  <dapp v-if="isReady" class="dapp-top" />
+  <div v-else />
 </template>
 
 <script lang="ts">
 import { useMeta } from 'quasar';
-import { defineComponent } from 'vue';
+import { defineComponent, ref, watchEffect } from 'vue';
 import Dapp from 'src/components/dapp-staking/dapp/Dapp.vue';
 
 export default defineComponent({
   components: { Dapp },
   setup() {
     useMeta({ title: 'Dapp Staking' });
+    const isReady = ref<boolean>(false);
+
+    // Memo: scrollBehavior in createRouter is not working (Vue Router)
+    // Memo: this is a quick hack to achieve 'scroll to top' whenever the page is opened from dApp page
+    const setIsReady = (): void => {
+      setTimeout(() => {
+        isReady.value = true;
+      }, 0);
+    };
+    watchEffect(setIsReady);
+    return { isReady };
   },
 });
 </script>
