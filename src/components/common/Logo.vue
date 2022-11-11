aa7ba6d0e8e4b3f3f4a734827c6810b93f32619f
Will <korea651@hanmail.net>
2022-11-11 21:55:49 +0900

    Fix - Shibuya logo appeared first before Astar logo (#578)
    * fix appear shiden logo first
    
    * prevent default astar logo

--- src/components/common/Logo.vue
+++ src/components/common/Logo.vue
@@ -1,45 +1,32 @@
 <template>
-  <div v-if="!small">
-    <div v-if="currentNetworkIdx === endpointKey.SHIDEN">
-      <img src="~assets/img/shiden_logo.png" widht="152" height="44" />
-    </div>
-    <div v-else-if="currentNetworkIdx === endpointKey.SHIBUYA">
-      <img src="~assets/img/shibuya_logo.png" width="152" height="55" />
-    </div>
-    <img v-else src="~assets/img/astar_logo.png" width="200" height="78" />
+  <div v-if="currentNetworkChain === astarChain.SHIDEN">
+    <img src="~assets/img/shiden_logo.png" widht="152" height="44" />
   </div>
-  <div v-else>
-    <img
-      v-if="currentNetworkIdx === endpointKey.SHIDEN"
-      src="~assets/img/shiden_logo.png"
-      alt="sdn"
-    />
-    <img
-      v-if="currentNetworkIdx === endpointKey.ASTAR"
-      src="~assets/img/astar_logo.png"
-      alt="astr"
-    />
+  <div v-else-if="currentNetworkChain === astarChain.SHIBUYA">
+    <img src="~assets/img/shibuya_logo.png" width="152" height="55" />
   </div>
+  <img
+    v-else-if="currentNetworkChain === astarChain.ASTAR"
+    src="~assets/img/astar_logo.png"
+    width="200"
+    height="78"
+  />
+  <div v-else></div>
 </template>
 <script lang="ts">
 import { endpointKey } from 'src/config/chainEndpoints';
 import { useNetworkInfo } from 'src/hooks';
-import { defineComponent, toRefs } from 'vue';
+import { defineComponent } from 'vue';
+import { astarChain } from 'src/config/chain';
 
 export default defineComponent({
-  props: {
-    small: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  setup(props) {
-    const { currentNetworkIdx } = useNetworkInfo();
+  setup() {
+    const { currentNetworkChain } = useNetworkInfo();
 
     return {
-      currentNetworkIdx,
+      currentNetworkChain,
       endpointKey,
-      ...toRefs(props),
+      astarChain,
     };
   },
 });
