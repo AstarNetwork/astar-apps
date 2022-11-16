36216247e064a60f40a407406de6ff5b958caf62
Bobo <bobo.kovacevic@gmail.com>
2022-11-16 21:23:41 +0100

    Light client basic implementation

--- src/config/api/polkadot/connectApi.ts
+++ src/config/api/polkadot/connectApi.ts
@@ -2,10 +2,15 @@ import { ApiPromise, WsProvider } from '@polkadot/api';
 import { endpointKey, providerEndpoints } from 'src/config/chainEndpoints';
 import { LOCAL_STORAGE } from 'src/config/localStorage';
 import { wait } from 'src/hooks/helper/common';
+import { ScProvider, WellKnownChain } from '@polkadot/rpc-provider/substrate-connect';
+import jsonParachainSpecAstar from './chain-specs/astar.json';
+import jsonParachainSpecShiden from './chain-specs/shiden.json';
 
 const RES_INVALID_CONNECTION = 'invalid connection';
 const RES_CONNECTED_API = 'connected';
 const RES_TIMEOUT = 'timeout';
+
+type Provider = WsProvider | ScProvider;
 
 // Memo: Reach to a healthy node whenever the selected endpoint has failed to connect to API
 const fallbackConnection = async ({
@@ -80,13 +85,31 @@ export async function connectApi(
 ): Promise<{
   api: ApiPromise;
 }> {
-  const provider = new WsProvider(endpoint);
-  const api = new ApiPromise({ provider });
+  // const provider = new WsProvider(endpoint);
+  // const api = new ApiPromise({ provider });
+  let provider: Provider;
+  let api: ApiPromise = new ApiPromise();
 
   store.commit('general/setCurrentNetworkStatus', 'connecting');
-  api.on('error', (error: Error) => console.error(error.message));
 
   try {
+    if (endpoint.startsWith('light')) {
+      const astarSpec = JSON.stringify(jsonParachainSpecAstar);
+      // const shidenSpec = JSON.stringify(jsonParachainSpecShiden);
+
+      const relayProvider = new ScProvider(WellKnownChain.polkadot);
+      provider = new ScProvider(astarSpec, relayProvider);
+      provider.on('error', (error: Error) => console.log('oh shit.....'));
+      provider.on('disconnected', (error: Error) => console.log('oh shit disconnected.....'));
+
+      await provider.connect();
+    } else {
+      provider = new WsProvider(endpoint);
+    }
+
+    api = new ApiPromise({ provider });
+    api.on('error', (error: Error) => console.error(error.message));
+
     const apiConnect = new Promise<string>((resolve) => {
       api.isReadyOrError.then(() => {
         resolve(RES_CONNECTED_API);
@@ -94,7 +117,7 @@ export async function connectApi(
     });
 
     const fallbackTimeout = new Promise<string>(async (resolve) => {
-      const timeout = 8 * 1000;
+      const timeout = endpoint.startsWith('light') ? 100 * 1000 : 8 * 1000;
       await wait(timeout);
       resolve(RES_TIMEOUT);
     });
