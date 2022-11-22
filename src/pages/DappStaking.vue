93651dabe634aadc10e442a9cec323b46a3a161d
impelcrypto <impelcrypto@gmail.com>
2022-11-22 18:40:49 +0800

    fix: cleanup

--- src/pages/DappStaking.vue
+++ src/pages/DappStaking.vue
@@ -12,19 +12,3 @@ export default defineComponent({
   setup() {},
 });
 </script>
-
-<style lang="scss" scoped>
-@use 'src/components/assets/styles/assets.scss';
-/* .wrapper--dapp-staking {
-  margin: 0 auto;
-}
-.container--dapp-staking {
-  display: grid;
-  row-gap: 24px;
-  max-width: 1300px;
-  margin: 0 auto;
-  @media (max-width: $md) {
-    display: block;
-  }
-} */
-</style>
