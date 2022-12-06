cde7509aff17ece667c90bc73a7af26d9e99844d
Bobo <bobo.kovacevic@gmail.com>
2022-12-06 16:28:23 +0100

    Claim error warning

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -210,6 +210,8 @@ export default {
     dappStakingEvm: 'dApp Staking is available on EVM',
     onChainData: 'On-Chain Data',
     stakingTvl: 'Staking TVL',
+    cantClaimWihtoutError:
+      'Looks like you are not able to claim at the moment because of too many unclaimed eras. Please turn off auto Re-Stake feature to be able to claim. After you claim rewards you can turn on re-stake again. UI team is working to fix this issue.',
     stakePage: {
       backToDappList: 'Back to dApps list',
       whereFundsFrom: 'Where would you like to bring you funds from?',
