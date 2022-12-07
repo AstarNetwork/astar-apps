3d23f6b645c03d11c5e2005aea9de2c51e49d504
Bobo <bobo.kovacevic@gmail.com>
2022-12-07 09:44:55 +0100

    Warning message update

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -211,7 +211,7 @@ export default {
     onChainData: 'On-Chain Data',
     stakingTvl: 'Staking TVL',
     cantClaimWihtoutError:
-      'Looks like you are not able to claim with automatic re-stake because you have not claimed in a while. Please turn off the Auto Re-Stake feature to be able to claim. After you claim rewards you can turn on re-stake again. UI team is working to fix this issue.',
+      'You cannot claim with automatic re-stake because it is been a while since you claimed your rewards. Please turn off the Auto Re-Stake feature to be able to claim. After you claimed rewards you can turn on re-stake again. The UI team is working to fix this issue.',
     stakePage: {
       backToDappList: 'Back to dApps list',
       whereFundsFrom: 'Where would you like to bring you funds from?',
