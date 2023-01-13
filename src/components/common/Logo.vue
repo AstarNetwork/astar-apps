13222306fe840a4bbba3d235e84b828ac8d69c85
Will <korea651@hanmail.net>
2023-01-13 20:46:10 +0900

    go back logo (#667)

--- src/components/common/Logo.vue
+++ src/components/common/Logo.vue
@@ -7,7 +7,7 @@
   </div>
   <img
     v-else-if="currentNetworkChain === astarChain.ASTAR"
-    src="~assets/img/astar_logo_winter.png"
+    src="~assets/img/astar_logo.png"
     width="200"
     height="78"
   />
