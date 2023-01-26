d0b4fdd979693821fd4627feab8337359c82aaaf
Bobo <bobo.kovacevic@gmail.com>
2023-01-26 14:07:09 +0100

    Display dapp stats (#669)

--- src/components/dapp-staking/dapp/Dapp.vue
+++ src/components/dapp-staking/dapp/Dapp.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="dapp" class="container--dapp-staking">
+  <div v-if="dapp && dapp.dapp" class="container--dapp-staking">
     <back-to-page :text="$t('dappStaking.stakePage.backToDappList')" :link="Path.DappStaking" />
     <dapp-avatar :dapp="dapp" />
     <dapp-statistics :dapp="dapp" />
@@ -9,8 +9,7 @@
       <project-overview :dapp="dapp" />
       <project-details :dapp="dapp" />
     </div>
-    <!-- Todo: uncomment when API is ready to get data -->
-    <!-- <dapp-stats-charts :dapp="dapp" /> -->
+    <dapp-stats-charts :dapp="dapp" />
     <div class="bottom--links">
       <router-link :to="buildStakePageLink(dapp.dapp.address)">
         <astar-irregular-button :disabled="isH160" :height="28" class="btn--stake-switch">
@@ -48,7 +47,7 @@ export default defineComponent({
     ProjectOverview,
     ProjectDetails,
     BackToPage,
-    // DappStatsCharts,
+    DappStatsCharts,
   },
   setup() {
     const route = useRoute();
