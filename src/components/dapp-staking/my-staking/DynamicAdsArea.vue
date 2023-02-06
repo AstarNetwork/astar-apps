04e0a5051a05061028dd0108d19665302e78c69e
Will <korea651@hanmail.net>
2023-02-06 20:21:23 +0900

    set separate developer inceptive card

--- src/components/dapp-staking/my-staking/DynamicAdsArea.vue
+++ src/components/dapp-staking/my-staking/DynamicAdsArea.vue
@@ -10,7 +10,11 @@
         </div>
         <div class="card-info">
           <div class="txt--category">{{ t.category }}</div>
-          <div class="txt--title">{{ t.title }}</div>
+          <div class="txt--title">
+            {{
+              index === 0 ? $n(truncate(rewardsDeveloper, 0)) + ' ' + nativeTokenSymbol : t.title
+            }}
+          </div>
           <div class="txt--subtitle">{{ t.subtitle }}</div>
         </div>
       </div>
@@ -18,9 +22,24 @@
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref, watchEffect } from 'vue';
+import { Struct } from '@polkadot/types';
+import { Perbill } from '@polkadot/types/interfaces';
+import { ethers } from 'ethers';
+import { $api } from 'src/boot/api';
+import { truncate } from 'src/hooks/helper/common';
+import { useNetworkInfo } from 'src/hooks';
 import NewsArea from './components/NewsArea.vue';
 import featuredData from 'src/data/featured_dapp.json';
+
+interface RewardDistributionConfig extends Struct {
+  readonly baseTreasuryPercent: Perbill;
+  readonly baseStakerPercent: Perbill;
+  readonly dappsPercent: Perbill;
+  readonly collatorsPercent: Perbill;
+  readonly adjustablePercent: Perbill;
+  readonly idealDappsStakingTvl: Perbill;
+}
 
 export default defineComponent({
   components: {
@@ -28,12 +47,36 @@ export default defineComponent({
   },
   setup() {
     const items = featuredData;
+    const rewardsDeveloper = ref<number>(0);
+    const { nativeTokenSymbol } = useNetworkInfo();
 
     const goToLink = (link: string) => {
       window.open(link, '_blank');
     };
 
+    const calcDeveloperRewards = async (): Promise<void> => {
+      try {
+        const api = $api!;
+        const [rewardsAllocation, rewardsAmount, blockPerEra] = await Promise.all([
+          api.query.blockReward.rewardDistributionConfigStorage<RewardDistributionConfig>(),
+          api.consts.blockReward.rewardAmount.toString(),
+          Number(api.consts.dappsStaking.blockPerEra.toString()),
+        ]);
+        const rewardsAmountPerEra = Number(ethers.utils.formatEther(rewardsAmount)) * blockPerEra;
+        const numAdjToPercentage = 0.000000001;
+        const dappsPercent = rewardsAllocation.dappsPercent.toNumber() * numAdjToPercentage;
+        rewardsDeveloper.value = rewardsAmountPerEra * dappsPercent;
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    watchEffect(calcDeveloperRewards);
+
     return {
+      nativeTokenSymbol,
+      rewardsDeveloper,
+      truncate,
       items,
       goToLink,
     };
