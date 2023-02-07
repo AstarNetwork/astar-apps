ba45eee193bb8c9eb4ffb2c16583167ca493992c
Will <korea651@hanmail.net>
2023-02-07 23:45:18 +0900

    update proper path

--- src/@types/ui-lib.d.ts
+++ src/@types/ui-lib.d.ts
@@ -1,4 +1,4 @@
-import { GlobalComponents as UILib } from '@astar-network/astar-ui';
+import { GlobalComponents as UILib } from '@astar-network/astar-ui/dist/packages/export_type';
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents extends UILib {}
