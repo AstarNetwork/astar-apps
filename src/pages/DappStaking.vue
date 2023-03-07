16c15240620d752b5d6a5e04c5d27ddb34a642a2
impelcrypto <impelcrypto@gmail.com>
2023-03-08 00:42:58 +0800

    feat: updated the condition for check isFaucet state

--- src/pages/DappStaking.vue
+++ src/pages/DappStaking.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="wrapper--dapp-staking">
+  <div v-if="isReady" class="wrapper--dapp-staking">
     <div class="container--dapp-staking">
       <router-view />
     </div>
@@ -7,8 +7,12 @@
 </template>
 
 <script lang="ts">
+import { usePageReady } from 'src/hooks';
 import { defineComponent } from 'vue';
 export default defineComponent({
-  setup() {},
+  setup() {
+    const { isReady } = usePageReady();
+    return { isReady };
+  },
 });
 </script>
