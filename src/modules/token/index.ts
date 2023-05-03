c958d87438b38665ad99ce8542fad9e7a0da2013
impelcrypto <impelcrypto@gmail.com>
2023-05-03 12:54:36 +0800

    feat: added ATOM and INJ on EVM assets page

--- src/modules/token/index.ts
+++ src/modules/token/index.ts
@@ -26,6 +26,7 @@ export interface Erc20Token {
   isWrappedToken?: boolean;
   isXC20?: boolean;
   wrapUrl?: string | null;
+  bridgeUrl?: string | null;
   userBalance?: string;
   userBalanceUsd?: string;
   isCbridgeToken?: boolean;
@@ -43,14 +44,34 @@ export const WASTR: Erc20Token = {
   wrapUrl: 'https://app.arthswap.org/#/swap',
 };
 
-export const registeredErc20Tokens: Erc20Token[] = [WASTR];
+export const ATOM: Erc20Token = {
+  srcChainId: Number(providerEndpoints[endpointKey.ASTAR].evmChainId),
+  address: '0xfFfFffff000000000000000000000000000186a2',
+  decimal: 6,
+  symbol: 'ATOM',
+  name: 'Cosmos Hub',
+  image: 'https://i.postimg.cc/zf4hRjfm/ATOM.png',
+  isWrappedToken: false,
+  isXC20: false,
+  bridgeUrl: 'https://cbridge.celer.network/999999997/592/ASTR',
+};
+
+export const INJ: Erc20Token = {
+  srcChainId: Number(providerEndpoints[endpointKey.ASTAR].evmChainId),
+  address: '0xFfFfFfFF000000000000000000000000000186a1',
+  decimal: 18,
+  symbol: 'INJ',
+  name: 'Injective',
+  image: 'https://i.postimg.cc/vZk23WdJ/INJtoken.png',
+  isWrappedToken: false,
+  isXC20: false,
+  bridgeUrl: 'https://cbridge.celer.network/999999997/592/INJ',
+};
+
+export const registeredErc20Tokens: Erc20Token[] = [WASTR, ATOM, INJ];
 
 // Memo: Define the token image source
 export const tokenImageMap = {
   ARSW: 'https://assets.coingecko.com/coins/images/26048/small/arsw.png?1655440190',
   LAY: 'https://assets.coingecko.com/coins/images/25795/small/WKBrkX4y_400x400.png?1653920060',
-};
-
-export const wrappedTokenMap = {
-  WASTR: 'ASTR',
 };
