da98297ffb9b967794142e3277a18a3a03bb590e
impelcrypto <impelcrypto@gmail.com>
2023-05-04 23:25:50 +0800

    feat: test log 6

--- src/hooks/wallet/useLedger.ts
+++ src/hooks/wallet/useLedger.ts
@@ -1,14 +1,15 @@
 import { Ledger } from '@polkadot/hw-ledger';
+import { useAccount } from 'src/hooks/useAccount';
 import { useStore } from 'src/store';
 import { computed, ref, watch } from 'vue';
 
 export const useLedger = () => {
   const isLedgerAccount = ref<boolean>(false);
   const isLedgerNanoS = ref<boolean>(false);
+  const { currentAccount } = useAccount();
 
   const store = useStore();
   const isH160 = computed<boolean>(() => store.getters['general/isH160Formatted']);
-  const currentAddress = computed<string>(() => store.getters['general/selectedAddress']);
 
   const handleReset = () => {
     isLedgerAccount.value = false;
@@ -16,7 +17,7 @@ export const useLedger = () => {
   };
 
   const handleLedgerData = async (): Promise<void> => {
-    if (currentAddress.value === null || isH160.value) {
+    if (!currentAccount.value || currentAccount.value === null || isH160.value) {
       handleReset();
       return;
     }
@@ -25,7 +26,7 @@ export const useLedger = () => {
       const { address } = await ledger.getAddress();
       const deviceModel = (ledger as any).__internal__app.transport.deviceModel;
       console.log('deviceModel', deviceModel);
-      isLedgerAccount.value = address === currentAddress.value;
+      isLedgerAccount.value = address === currentAccount.value;
       isLedgerNanoS.value = deviceModel.id === 'nanoS';
       console.log('isLedgerAccount.value', isLedgerAccount.value);
       console.log('isLedgerNanoS.value', isLedgerNanoS.value);
@@ -34,7 +35,7 @@ export const useLedger = () => {
     }
   };
 
-  watch([currentAddress], handleLedgerData, { immediate: true });
+  watch([currentAccount], handleLedgerData, { immediate: true });
 
   return { isLedgerAccount, isLedgerNanoS };
 };
