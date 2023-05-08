fbf0c9b7a4786c8611800f6aceb852582d19ab0f
impelcrypto <impelcrypto@gmail.com>
2023-05-08 16:35:28 +0800

    refactor: removed console logs

--- src/hooks/wallet/useLedger.ts
+++ src/hooks/wallet/useLedger.ts
@@ -3,6 +3,12 @@ import { useAccount } from 'src/hooks/useAccount';
 import { useStore } from 'src/store';
 import { computed, ref, watch } from 'vue';
 
+enum LedgerId {
+  nanoS = 'nanoS',
+  nanoSPlus = 'nanoSP',
+  nanoX = 'nanoX',
+}
+
 export const useLedger = () => {
   const isLedgerAccount = ref<boolean>(false);
   const isLedgerNanoS = ref<boolean>(false);
@@ -25,11 +31,8 @@ export const useLedger = () => {
       const ledger = new Ledger('hid', 'astar');
       const { address } = await ledger.getAddress();
       const deviceModel = (ledger as any).__internal__app.transport.deviceModel;
-      console.log('deviceModel', deviceModel);
       isLedgerAccount.value = address === currentAccount.value;
-      isLedgerNanoS.value = deviceModel.id === 'nanoS';
-      console.log('isLedgerAccount.value', isLedgerAccount.value);
-      console.log('isLedgerNanoS.value', isLedgerNanoS.value);
+      isLedgerNanoS.value = deviceModel.id === LedgerId.nanoS;
     } catch (error) {
       handleReset();
     }
