aad93688f2028b263dfea85d40a672a58a160876
Gregory Luneau <gregory.luneau@gmail.com>
2023-05-09 10:18:42 -0500

    Using the last post of astar blog as the 4th card

--- src/components/assets/DynamicLinks.vue
+++ src/components/assets/DynamicLinks.vue
@@ -5,7 +5,7 @@
         v-for="(t, index) in items"
         :key="index"
         class="card"
-        :style="`background-image: url('${t.background}');`"
+        :style="`background-image: url('${t.background}'); background-size: cover; background-position: center;`"
         @click="goToLink(t.link)"
       >
         <div class="card--info">
@@ -19,9 +19,18 @@
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, ref, watchEffect, computed } from 'vue';
+import { defineComponent, ref, watch } from 'vue';
 import { truncate } from '@astar-network/astar-sdk-core';
 import linksData from 'src/data/dynamic_links.json';
+import { useQuery } from '@vue/apollo-composable';
+import gql from 'graphql-tag';
+
+interface Data {
+  background: string;
+  title: string;
+  subtitle: string;
+  link: string;
+}
 
 export default defineComponent({
   components: {},
@@ -30,14 +39,46 @@ export default defineComponent({
       require('/src/assets/img/banner/banner01.svg'),
       require('/src/assets/img/banner/banner02.svg'),
       require('/src/assets/img/banner/banner03.svg'),
-      require('/src/assets/img/banner/banner04.svg'),
     ];
-    const items = linksData.map((item, index) => ({
+
+    // The subsocial space where the news updates come from: https://polkaverse.com/10802
+    const astarSpace = 10802;
+    const item = ref<Data[]>([]);
+    const { result, loading, error } = useQuery(gql`
+      query PostsBySpaceId {
+        posts(where: { space: { id_eq: "${astarSpace}" }, AND: { hidden_not_eq: true } }, orderBy: id_DESC, limit: 1) {
+          background: image
+          title
+          link: slug
+        }
+      }
+    `);
+
+    let items = linksData.map((item, index) => ({
       background: banners[index],
       title: item.title,
       subtitle: item.subtitle,
       link: item.link,
     }));
+
+    watch(
+      [result, error],
+      async () => {
+        if (result.value) {
+          item.value = result.value.posts.map((x: Data) => {
+            return {
+              background: `https://ipfs.subsocial.network/ipfs/${x.background}`,
+              title: '', // x.title,
+              subtitle: '',
+              link: `https://astar.network/blog/${x.link}`,
+            };
+          });
+          console.log('item.value[0]', item.value[0]);
+          items.push(item.value[0]);
+        }
+      },
+      { immediate: true }
+    );
 
     const goToLink = (link: string) => {
       window.open(link, '_blank');
