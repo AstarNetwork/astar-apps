d10ee59ba343a7d3ea0314b846a93a56809eab5f
Gregory Luneau <gregory.luneau@gmail.com>
2023-05-18 07:19:43 -0500

    draft of test XCM006

---
+++ tests/reserve-transfer-asset-to-parachain.spec.ts
@@ -0,0 +1,61 @@
+import { clickPolicyButton } from 'src/modules/playwright';
+import { expect } from '@playwright/test';
+import { test } from './fixtures';
+import {
+  ALICE_ACCOUNT_NAME,
+  ALICE_ACCOUNT_SEED,
+  ALICE_ADDRESS,
+  closePolkadotWelcomePopup,
+  connectToNetwork,
+  createAccount,
+  selectAccount,
+  signTransaction,
+} from './common';
+import { ApiPromise } from '@polkadot/api';
+import { chainDecimals, getApi, getBalance } from './common-api';
+
+let api: ApiPromise;
+test.beforeAll(async () => {
+  api = await getApi();
+});
+
+test.afterAll(async () => {
+  await api.disconnect();
+});
+
+test.beforeEach(async ({ page, context }) => {
+  // TODO consider moving this into beforeAll
+  await page.goto('/astar/assets');
+  await clickPolicyButton(page);
+  const closeButton = page.getByText('Polkadot.js');
+  await closeButton.click();
+
+  await closePolkadotWelcomePopup(context);
+  await createAccount(page, ALICE_ACCOUNT_SEED, ALICE_ACCOUNT_NAME);
+  await page.goto('/astar/assets');
+  await connectToNetwork(page);
+  await selectAccount(page, ALICE_ACCOUNT_NAME);
+});
+
+test.describe('account panel', () => {
+  // Test case: XCM006
+  test('should transfer ASTR tokens from Alice to Acala', async ({ page, context }) => {
+    const transferAmount = BigInt(1000);
+    await page.locator('.icon--expand').first().click();
+    await page.locator('#asset-expand').getByRole('button', { name: 'Transfer' }).click();
+
+    await page.getByPlaceholder('Destination Address').fill(ALICE_ADDRESS);
+    await page.getByPlaceholder('0.0').fill(transferAmount.toString());
+    await page.getByRole('button', { name: 'Confirm' }).click();
+
+    const bobBalanceBeforeTransaction = await getBalance(ALICE_ADDRESS);
+    await signTransaction(context);
+    await page.waitForSelector('.four', { state: 'hidden' });
+
+    await expect(page.getByText('Success')).toBeVisible();
+    const bobBalanceAfterTransaction = await getBalance(ALICE_ADDRESS);
+    expect(bobBalanceAfterTransaction - bobBalanceBeforeTransaction).toEqual(
+      transferAmount * BigInt(Math.pow(10, chainDecimals))
+    );
+  });
+});
