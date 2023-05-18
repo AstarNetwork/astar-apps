09a80c8200a4f89d3f2a36f3ef1e84885f98033c
impelcrypto <impelcrypto@gmail.com>
2023-05-18 17:43:19 +0800

    fix: modified claim rewards box

--- src/hooks/useClaimAll.ts
+++ src/hooks/useClaimAll.ts
@@ -53,6 +53,12 @@ export function useClaimAll() {
     return Number(balance);
   });
 
+  const isDappDeveloper = computed<boolean>(() => {
+    return (
+      dapps.value && dapps.value.some((it) => it.contract.developerAddress === senderAddress.value)
+    );
+  });
+
   const updateClaimEras = async (): Promise<void> => {
     try {
       isLoading.value = true;
@@ -64,7 +70,6 @@ export function useClaimAll() {
       if (!senderAddressRef || !era.value || isSendingTx.value) {
         return;
       }
-
       const txs = await Promise.all(
         dapps.value.map(async (it) => {
           if (it.dapp && !isH160.value) {
@@ -168,5 +173,6 @@ export function useClaimAll() {
     canClaimWithoutError,
     isLoading,
     amountOfEras,
+    isDappDeveloper,
   };
 }
