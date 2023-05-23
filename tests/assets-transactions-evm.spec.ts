95ae4311be06ade0e4cdf86ba14a13bb45b5682c
Will <korea651@hanmail.net>
2023-05-23 20:48:23 +0900

    update evm process

--- tests/assets-transactions-evm.spec.ts
+++ tests/assets-transactions-evm.spec.ts
@@ -47,37 +47,39 @@ test.beforeEach(async ({ page, context }) => {
   );
   await page.goto('/astar/assets');
   await connectToNetwork(page);
+  await selectAccount(page, ALICE_ACCOUNT_NAME);
 });
 
 test.describe('account panel', () => {
   // Test case: AS002
   test('should transfer tokens from Alice to Bob on EVM account', async ({ page, context }) => {
     // transfer test (from native to evm) :: need to testing
-    await page.goto('/custom-node/assets/transfer?token=astr&mode=local');
+    await page.locator('.icon--expand').first().click();
+    await page.locator('#asset-expand').getByRole('button', { name: 'Transfer' }).click();
     const faucetAmount = BigInt(200);
     await page.getByPlaceholder('Destination Address').fill(ALICE_EVM_ADDRESS);
     await page.getByPlaceholder('0.0').fill(faucetAmount.toString());
+    await page.locator('.box--warning label').check();
     await page.getByRole('button', { name: 'Confirm' }).click();
     await signTransaction(context);
     await page.waitForSelector('.four', { state: 'hidden' });
     await expect(page.getByText('Success')).toBeVisible();
 
     //metamask setup
+    await page.locator('.btn--account').click();
+    await page.locator('.wrapper--modal-drawer .modal-close').first().click();
     await page.getByText('MetaMask').click();
     await signInMetamask(context);
 
+    //transfer test (from evm to native)
+    await page.goto('/custom-node/assets/transfer?token=astr&mode=local');
     await page.locator('.btn--connect').click();
     await page.getByText('MetaMask').click();
-
     await connectWithEVM(page, context);
     // TBD: change network on EVM :: need to testing
     // await changeNetworkOnEVM(page, context);
-
     await page.waitForSelector('.modal-close', { state: 'hidden' });
     await expect(page.getByText('Select a Wallet')).toBeHidden();
-
-    //transfer test (from evm to native)
-    await page.goto('/custom-node/assets/transfer?token=astr&mode=local');
 
     const baseTransferAmount = BigInt(10);
     //BOB_ADDRESS > ALICE_EVM_ADDRESS
