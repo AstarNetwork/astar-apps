af03e553490ff9cbe603731ff05184276dba2a4a
Ting Tze Chuen <amdcopper@gmail.com>
2023-05-26 14:54:27 +0800

    Add Hana wallet support (#563)
    * Add Hana wallet
    
    * WalletModalOption and ethExtension updates
    
    ---------
    
    Co-authored-by: MJ <3699611+mj-hana@users.noreply.github.com>

--- src/config/wallets.ts
+++ src/config/wallets.ts
@@ -19,6 +19,8 @@ export enum SupportWallet {
   SubWalletEvm = 'SubWallet',
   Metadot = 'metadot',
   Wallet3 = 'wallet3',
+  HanaNative = 'hana',
+  HanaEvm = 'hanaEvm',
 }
 
 export const WalletModalOption = {
@@ -38,6 +40,8 @@ export const WalletModalOption = {
   SubWalletEvm: SupportWallet.SubWalletEvm,
   Metadot: SupportWallet.Metadot,
   Wallet3: SupportWallet.Wallet3,
+  HanaNative: SupportWallet.HanaNative,
+  HanaEvm: SupportWallet.HanaEvm,
 };
 
 export const SubstrateWallets = [
@@ -50,6 +54,7 @@ export const SubstrateWallets = [
   SupportWallet.SubWalletNative,
   SupportWallet.Metadot,
   SupportWallet.Wallet3,
+  SupportWallet.HanaNative,
 ];
 
 export interface Wallet {
@@ -137,6 +142,15 @@ export const supportWalletObj = {
     isSupportBrowserExtension: true,
     isSupportMobileApp: false,
   },
+  [SupportWallet.HanaNative]: {
+    img: require('/src/assets/img/logo-hana.png'),
+    name: 'Hana Wallet (Native)',
+    source: SupportWallet.HanaNative,
+    walletUrl: 'https://hanawallet.io/',
+    guideUrl: 'https://hanawallet.io/',
+    isSupportBrowserExtension: true,
+    isSupportMobileApp: false,
+  },
 };
 
 export const supportEvmWalletObj = {
@@ -189,6 +203,16 @@ export const supportEvmWalletObj = {
     isSupportBrowserExtension: false,
     isSupportMobileApp: true,
     ethExtension: 'ethereum',
+  },
+  [SupportWallet.HanaEvm]: {
+    img: require('/src/assets/img/logo-hana.png'),
+    name: 'Hana Wallet (EVM)',
+    source: SupportWallet.HanaEvm,
+    walletUrl: 'https://hanawallet.io/',
+    guideUrl: 'https://hanawallet.io/',
+    isSupportBrowserExtension: true,
+    isSupportMobileApp: false,
+    ethExtension: 'hanaWalletEth',
   },
 };
 
