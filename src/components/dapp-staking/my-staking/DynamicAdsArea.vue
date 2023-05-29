92b1fecbe048b224f0e3797402a38b20ced71edf
Will <korea651@hanmail.net>
2023-05-29 22:57:29 +0900

    update details

--- src/components/dapp-staking/my-staking/DynamicAdsArea.vue
+++ src/components/dapp-staking/my-staking/DynamicAdsArea.vue
@@ -95,8 +95,9 @@ export default defineComponent({
       //   } else {
       //     return require('/src/assets/img/ic_astar_farm.svg');
       //   }
-      // } else
-      if (index === 1) {
+      if (index === 0) {
+        return require('/src/assets/img/paras_logo.svg');
+      } else if (index === 1) {
         return require('/src/assets/img/ic_algem_staking.svg');
       } else if (index === 2) {
         return require('/src/assets/img/ic_subwallet.svg');
