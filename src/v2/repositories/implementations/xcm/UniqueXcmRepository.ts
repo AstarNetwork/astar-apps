508e737274d589f812c0509b241c1efbf97eb6df
Andy Smith <59873862+uandysmith@users.noreply.github.com>
2023-05-30 05:23:21 +0600

    Unique asset support (#811)

---
+++ src/v2/repositories/implementations/xcm/UniqueXcmRepository.ts
@@ -0,0 +1,68 @@
+import { BN } from '@polkadot/util';
+import { decodeAddress } from '@polkadot/util-crypto';
+import { Asset } from 'src/v2/models';
+import { ExtrinsicPayload, IApi, IApiFactory } from 'src/v2/integration';
+import { XcmChain } from 'src/v2/models/XcmModels';
+import { XcmRepository } from 'src/v2/repositories/implementations/XcmRepository';
+import { XcmTokenInformation } from 'src/modules/xcm';
+import { container } from 'src/v2/common';
+import { Symbols } from 'src/v2/symbols';
+
+export class UniqueXcmRepository extends XcmRepository {
+  constructor() {
+    const defaultApi = container.get<IApi>(Symbols.DefaultApi);
+    const apiFactory = container.get<IApiFactory>(Symbols.ApiFactory);
+    const registeredTokens = container.get<XcmTokenInformation[]>(Symbols.RegisteredTokens);
+    super(defaultApi, apiFactory, registeredTokens);
+  }
+
+  public async getTransferCall(
+    from: XcmChain,
+    to: XcmChain,
+    recipientAddress: string,
+    token: Asset,
+    amount: BN
+  ): Promise<ExtrinsicPayload> {
+    if (!to.parachainId) {
+      throw `Parachain id for ${to.name} is not defined`;
+    }
+    if (token.id !== '18446744073709551631') {
+      throw 'Token must be UNQ';
+    }
+
+    const tokenData = { NativeAssetId: 'Here' };
+
+    const destination = {
+      V2: {
+        parents: '1',
+        interior: {
+          X2: [
+            {
+              Parachain: to.parachainId,
+            },
+            {
+              AccountId32: {
+                network: {
+                  Any: null,
+                },
+                id: decodeAddress(recipientAddress),
+              },
+            },
+          ],
+        },
+      },
+    };
+
+    const destWeight = { Unlimited: null };
+
+    return await this.buildTxCall(
+      from,
+      'xTokens',
+      'transfer',
+      tokenData,
+      amount,
+      destination,
+      destWeight
+    );
+  }
+}
