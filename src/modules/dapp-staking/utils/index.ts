54763c34ff1aedd0716f48e9a8852bb93dbc9a2a
impelcrypto <impelcrypto@gmail.com>
2023-05-31 22:07:29 +0800

    refactor: clean up

--- src/modules/dapp-staking/utils/index.ts
+++ src/modules/dapp-staking/utils/index.ts
@@ -1,17 +1,10 @@
 import { LOCAL_STORAGE } from 'src/config/localStorage';
 
 export const checkIsLimitedProvider = (): boolean => {
-  const limitedProvider = ['onfinality', 'blast'];
+  const limitedProvider = ['onfinality', 'blastapi'];
   const selectedEndpoint = JSON.parse(
     String(localStorage.getItem(LOCAL_STORAGE.SELECTED_ENDPOINT))
   );
   const endpoint = String(Object.values(selectedEndpoint)[0]);
-  let result = false;
-  limitedProvider.forEach((it) => {
-    const res = endpoint.includes(it);
-    if (res) {
-      result = true;
-    }
-  });
-  return result;
+  return limitedProvider.some((it) => endpoint.includes(it));
 };
