b85ec820e3ad67486c3010a8120ccee07b4ac99e
Will <korea651@hanmail.net>
2023-06-01 18:15:04 +0900

    update modal disclaimer

--- src/components/common/ModalDisclaimer.vue
+++ src/components/common/ModalDisclaimer.vue
@@ -2,60 +2,45 @@
   <astar-default-modal
     v-if="show"
     :show="show"
-    :title="'Disclaimer'"
+    :title="$t('disclaimer.disclaimer')"
     :is-closing="isClosingModal"
     :width="650"
     :class="'highest-z-index'"
-    @close="setIsOpen(false)"
+    @close="decline()"
   >
     <div class="wrapper--modal-disclaimer">
       <div class="row--description">
         <div class="text--md">
-          By accessing and using the Astar ecosystem dApps, you confirm that you are eligible to do
-          so and agree to the Astar Foundation Terms of Service and Privacy Policy. You understand
-          and accept that your use of the dApps is at your own risk, and they are provided on an "as
-          is" and "as available" basis without any express or implied warranties of any kind.
+          {{ $t('disclaimer.introduce') }}
         </div>
-        <div class="text--title">Terms of Services</div>
-        <div class="text--lg">1. Liability:</div>
+        <div class="text--title">{{ $t('disclaimer.terms') }}</div>
+        <div class="text--lg">{{ $t('disclaimer.chapter1') }}</div>
         <div class="text--md">
-          You understand and agree that the Astar Foundation shall not be held liable for any
-          direct, indirect, incidental, special, consequential, or exemplary damages arising from
-          your use of the Astar ecosystem dApps. This includes, but is not limited to, damages for
-          loss of profits, goodwill, use, data or other intangible losses.
+          {{ $t('disclaimer.para1') }}
         </div>
-        <div class="text--lg">2. Assumption of Network Risks:</div>
+        <div class="text--lg">{{ $t('disclaimer.chapter2') }}</div>
         <div class="text--md">
-          You understand and agree that the Astar Foundation shall not be held liable for any
-          direct, indirect, incidental, special, consequential, or exemplary damages arising from
-          your use of the Astar ecosystem dApps. This includes, but is not limited to, damages for
-          loss of profits, goodwill, use, data or other intangible losses.
+          {{ $t('disclaimer.para1') }}
         </div>
-        <div class="text--lg">3. No Guarantee of Security:</div>
+        <div class="text--lg">{{ $t('disclaimer.chapter3') }}</div>
         <div class="text--md">
-          You understand and agree that the Astar Foundation does not guarantee the security of the
-          Astar ecosystem dApps or the safety of your personal information, including your wallet
-          address, transaction history, and private keys. You agree to take appropriate measures to
-          protect your personal information and to prevent unauthorized access to your wallet.
+          {{ $t('disclaimer.para2') }}
         </div>
         <div class="text--md">
-          You also confirm that you are not a "Prohibited Person" and that neither you nor any
-          person or entity that controls, is controlled by, or is under common control with you is a
-          Prohibited Person. Finally, by clicking "Accept," you agree to the terms and conditions
-          set forth above. If you do not agree, please click "Decline."
+          {{ $t('disclaimer.closeWord') }}
         </div>
       </div>
 
       <div class="container--buttons">
         <div class="row--button">
           <astar-button class="btn--action" @click="accept()">
-            <span class="text--button">Agree</span>
+            <span class="text--button">{{ $t('disclaimer.agree') }}</span>
           </astar-button>
         </div>
         <div class="row--button">
-          <astar-button class="btn--action btn--decline" @click="setIsOpen(false)"
-            >Decline</astar-button
-          >
+          <astar-button class="btn--action btn--decline" @click="decline()">{{
+            $t('disclaimer.decline')
+          }}</astar-button>
         </div>
       </div>
     </div>
@@ -98,12 +83,17 @@ export default defineComponent({
       closeModal();
     };
 
+    const decline = () => {
+      window.open('https://astar.network/', '_self');
+    };
+
     return {
       close,
       truncate,
       closeModal,
       isClosingModal,
       accept,
+      decline,
     };
   },
 });
