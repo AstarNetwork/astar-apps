4849f4d6fc25c13e92ee4298a777ff819ba3262e
impelcrypto <impelcrypto@gmail.com>
2023-06-05 20:51:12 +0800

    feat: added test

--- tests/dashboard.spec.ts
+++ tests/dashboard.spec.ts
@@ -1,4 +1,6 @@
 import { expect, test } from '@playwright/test';
+import { endpointKey } from 'src/config/chainEndpoints';
+import { providerEndpoints } from 'src/config/chainEndpoints';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('/astar/dashboard');
@@ -7,5 +9,19 @@ test.beforeEach(async ({ page }) => {
 test.describe('on dashboard screen', () => {
   test('has title', async ({ page }) => {
     await expect(page).toHaveTitle(/Dashboard/);
+  });
+  test('Endpoint has been selected randomly', async ({ page }) => {
+    const isAppliedRandomEndpoint = await page.evaluate(() => {
+      return localStorage.getItem('isAppliedRandomEndpoint');
+    });
+    const selectedEndpointObj: string = (await page.evaluate(() => {
+      return localStorage.getItem('selectedEndpoint');
+    })) as string;
+    const selectedEndpoint = JSON.parse(selectedEndpointObj)[0];
+    const isSomeOfAstarEndpoints = providerEndpoints[endpointKey.ASTAR].endpoints.some(
+      (it) => it.endpoint === selectedEndpoint
+    );
+    expect(isAppliedRandomEndpoint).toBe('true');
+    expect(isSomeOfAstarEndpoints).toBe(true);
   });
 });
