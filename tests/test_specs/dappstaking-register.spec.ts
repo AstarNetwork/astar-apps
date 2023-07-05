9013fac4108fdf038c604a8ea5ffdd1f74286771
Bobo <bobo.kovacevic@gmail.com>
2023-07-05 13:50:44 +0200

    Test improvements

--- tests/test_specs/dappstaking-register.spec.ts
+++ tests/test_specs/dappstaking-register.spec.ts
@@ -1,4 +1,4 @@
-import { BrowserContext, Page } from '@playwright/test';
+import { BrowserContext, Page, expect } from '@playwright/test';
 import { test } from '../fixtures';
 import {
   BOB_ACCOUNT_NAME,
@@ -39,10 +39,16 @@ test.beforeEach(async ({ page, context }: { page: Page; context: BrowserContext 
 
 test.describe('register dApp page', () => {
   test('validate builder image size', async ({ page }) => {
-    // await page.goto('/custom-node/dapp-staking/discover');
-    // await selectAccount(page, BOB_ACCOUNT_NAME);
     await page.getByRole('link', { name: '🎉 Congrats!! your contract is approved. Please submit the details Register now' }).click();
     await page.locator('.builders--container > .card > .card--image > .slot > .card-new-item').click();
     await page.getByLabel('Builder\'s image (maximum upload file size: 30KB)').isVisible();
+    // Upload an image that is too large.
+    await page.getByLabel('Builder\'s image (maximum upload file size: 30KB)').setInputFiles('./tests/assets/invalid_developer_image.jpeg');
+    // Image didn't show up on UI since it is too big. Add image button should be displayed.
+    await expect(page.locator('.builders--container > .card > .card--image > .slot > .image')).toBeHidden();
+    // Upload a valid image.
+    await page.getByLabel('Builder\'s image (maximum upload file size: 30KB)').setInputFiles('./tests/assets/valid_developer_image.jpeg');
+    // Check if image is displayed.
+    await expect(page.locator('.builders--container > .card > .card--image > .image')).toBeVisible();
   });
 });
