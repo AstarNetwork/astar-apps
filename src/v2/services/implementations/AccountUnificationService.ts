177f24f47a776c0f08d6146b02e8e808401fe939
Bobo <bobo.kovacevic@gmail.com>
2023-09-12 15:51:17 +0200

    Error handling

--- src/v2/services/implementations/AccountUnificationService.ts
+++ src/v2/services/implementations/AccountUnificationService.ts
@@ -3,8 +3,7 @@ import { Symbols } from 'src/v2/symbols';
 import { IWalletService, WalletType, IAccountUnificationService } from 'src/v2/services';
 import { IAccountsRepository, ISystemRepository } from 'src/v2/repositories';
 import { decodeAddress } from '@polkadot/util-crypto';
-import { u8aToHex } from '@polkadot/util';
-import { ethers } from 'ethers';
+import { ExtrinsicStatusMessage, IEventAggregator } from 'src/v2/messaging';
 
 @injectable()
 export class AccountUnificationService implements IAccountUnificationService {
@@ -12,38 +11,52 @@ export class AccountUnificationService implements IAccountUnificationService {
     @inject(Symbols.WalletFactory)
     private walletFactory: (walletType?: WalletType) => IWalletService,
     @inject(Symbols.SystemRepository) private systemRepo: ISystemRepository,
-    @inject(Symbols.AccountsRepository) private accountsRepo: IAccountsRepository
+    @inject(Symbols.AccountsRepository) private accountsRepo: IAccountsRepository,
+    @inject(Symbols.EventAggregator) private eventAggregator: IEventAggregator
   ) {}
 
   public async unifyAccounts(ss58Address: string, h160Address: string): Promise<void> {
-    const chainId = await this.systemRepo.getChainId();
-    const genesisHash = await this.systemRepo.getBlockHash(0);
-    const publicKey = decodeAddress(ss58Address);
+    try {
+      const chainId = await this.systemRepo.getChainId();
+      const genesisHash = await this.systemRepo.getBlockHash(0);
+      const publicKey = decodeAddress(ss58Address);
 
-    const domain = {
-      chainId: chainId,
-      name: 'Astar EVM Claim',
-      version: '1',
-      salt: genesisHash,
-    };
-    const types = {
-      Claim: [{ name: 'substrateAddress', type: 'bytes' }],
-    };
-    const value = {
-      substrateAddress: publicKey,
-    };
+      const domain = {
+        chainId: chainId,
+        name: 'Astar EVM Claim',
+        version: '1',
+        salt: genesisHash,
+      };
+      const types = {
+        Claim: [{ name: 'substrateAddress', type: 'bytes' }],
+      };
+      const value = {
+        substrateAddress: publicKey,
+      };
 
-    // Sign payload with EVM account.
-    const evmWallet = this.walletFactory(WalletType.Metamask);
-    const signedPayload = await evmWallet.signPayload(domain, types, value);
+      // Sign payload with EVM account.
+      const evmWallet = this.walletFactory(WalletType.Metamask);
+      const signedPayload = await evmWallet.signPayload(domain, types, value);
 
-    // Claim account with polkadot wallet.
-    const transaction = await this.accountsRepo.getClaimEvmAccountCall(h160Address, signedPayload);
-    const polkadotWallet = this.walletFactory(WalletType.Polkadot);
+      // Claim account with polkadot wallet.
+      const transaction = await this.accountsRepo.getClaimEvmAccountCall(
+        h160Address,
+        signedPayload
+      );
+      const polkadotWallet = this.walletFactory(WalletType.Polkadot);
 
-    await polkadotWallet.signAndSend({
-      extrinsic: transaction,
-      senderAddress: ss58Address,
-    });
+      await polkadotWallet.signAndSend({
+        extrinsic: transaction,
+        senderAddress: ss58Address,
+      });
+    } catch (error) {
+      const e = error as Error;
+      this.eventAggregator.publish(
+        new ExtrinsicStatusMessage({
+          success: false,
+          message: e.toString(),
+        })
+      );
+    }
   }
 }
