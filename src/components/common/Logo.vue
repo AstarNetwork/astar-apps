ff6c6584d4c1fddf6de890dc59ed0b24d4a7c8b2
impelcrypto <impelcrypto@gmail.com>
2023-10-11 17:24:10 +0800

    fix: clean up

--- src/components/common/Logo.vue
+++ src/components/common/Logo.vue
@@ -2,21 +2,11 @@
   <img src="~assets/img/astar_logo.png" width="200" height="78" />
 </template>
 <script lang="ts">
-import { endpointKey, providerEndpoints } from 'src/config/chainEndpoints';
-import { useNetworkInfo } from 'src/hooks';
 import { defineComponent } from 'vue';
-import { astarChain } from 'src/config/chain';
 
 export default defineComponent({
   setup() {
-    const { currentNetworkChain } = useNetworkInfo();
-
-    return {
-      providerEndpoints,
-      currentNetworkChain,
-      endpointKey,
-      astarChain,
-    };
+    return {};
   },
 });
 </script>
