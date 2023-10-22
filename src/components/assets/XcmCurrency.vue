b4da5a6dc73b2e54768daf8f3469340ce1e1faa5
Ayumi Takahashi <ayumee528@gmail.com>
2023-10-22 03:06:50 -0700

    native token 2

--- src/components/assets/XcmCurrency.vue
+++ src/components/assets/XcmCurrency.vue
@@ -1,76 +1,69 @@
 <template>
   <div v-if="isDisplayToken">
-    <div class="border--separator" />
-    <div class="rows">
-      <div class="row row--details">
-        <div class="row__left">
-          <div class="column--currency">
-            <div class="token-logo">
-              <jazzicon
-                v-if="token.tokenImage.includes('custom-token')"
-                :address="token.mappedERC20Addr"
-                :diameter="24"
-              />
-              <img v-else :src="token.tokenImage" alt="logo" />
-            </div>
-            <div class="column--ticker">
-              <span class="text--title">{{ token.metadata.symbol }}</span>
-              <span class="text--label">{{ token.metadata.name }}</span>
-            </div>
+    <div class="row" :class="isExpand && 'row--is-expand'">
+      <div class="row__left" @click="isExpand = !isExpand">
+        <div class="column--token">
+          <div class="icon--token">
+            <jazzicon
+              v-if="token.tokenImage.includes('custom-token')"
+              :address="token.mappedERC20Addr"
+              :diameter="24"
+            />
+            <img v-else :src="token.tokenImage" alt="logo" />
+          </div>
+          <div>
+            <div class="text--title">{{ token.metadata.symbol }}</div>
+            <div class="text--label">{{ token.metadata.name }}</div>
           </div>
         </div>
-        <div class="row__right">
-          <div class="column column--balance">
-            <div class="column__box">
-              <div class="text--accent">
-                <span>
-                  <token-balance
-                    :balance="String(token.userBalance)"
-                    :symbol="token.metadata.symbol"
-                  />
-                </span>
-              </div>
-              <div class="text--label">
-                <span>{{ $n(Number(token.userBalanceUsd)) }} {{ $t('usd') }}</span>
-              </div>
-            </div>
+
+        <div class="column--balance">
+          <div class="column--balance__row text--title">
+            <token-balance :balance="String(token.userBalance)" :symbol="token.metadata.symbol" />
           </div>
-          <div class="column--asset-buttons column--buttons--native">
-            <router-link :to="buildTransferPageLink(token.metadata.symbol)">
-              <button class="btn btn--sm">
-                {{ $t('assets.transfer') }}
-              </button>
-            </router-link>
-            <div class="screen--xl">
-              <a
-                class="box--explorer"
-                :href="explorerLink"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <button class="btn btn--sm adjuster--width">
-                  <div class="container--explorer-icon adjuster--width">
-                    <astar-icon-external-link />
-                  </div>
-                </button>
-              </a>
-              <q-tooltip>
-                <span class="text--tooltip">{{ $t('subscan') }}</span>
-              </q-tooltip>
-            </div>
+          <div class="column--balance__row text--label">
+            {{ $n(Number(token.userBalanceUsd)) }} {{ $t('usd') }}
           </div>
         </div>
       </div>
+
+      <q-slide-transition :duration="150">
+        <div v-show="isExpand || width >= screenSize.sm" class="row__right">
+          <div>
+            <router-link :to="buildTransferPageLink(token.metadata.symbol)">
+              <button class="btn btn--icon">
+                <astar-icon-transfer />
+              </button>
+            </router-link>
+            <span class="text--expand-menu">{{ $t('assets.send') }}</span>
+            <q-tooltip>
+              <span class="text--tooltip">{{ $t('assets.send') }}</span>
+            </q-tooltip>
+          </div>
+
+          <div>
+            <a :href="explorerLink" target="_blank" rel="noopener noreferrer">
+              <button class="btn btn--icon">
+                <astar-icon-external-link class="icon--external-link" />
+              </button>
+            </a>
+            <span class="text--expand-menu">{{ $t('subscan') }}</span>
+            <q-tooltip>
+              <span class="text--tooltip">{{ $t('subscan') }}</span>
+            </q-tooltip>
+          </div>
+        </div>
+      </q-slide-transition>
     </div>
   </div>
 </template>
 <script lang="ts">
 import { endpointKey } from 'src/config/chainEndpoints';
-import { useNetworkInfo } from 'src/hooks';
+import { useNetworkInfo, useBreakpoints } from 'src/hooks';
 import { getXcmToken } from 'src/modules/xcm';
 import { buildTransferPageLink } from 'src/router/routes';
 import { Asset } from 'src/v2/models';
-import { computed, defineComponent, PropType } from 'vue';
+import { computed, defineComponent, PropType, ref } from 'vue';
 import Jazzicon from 'vue3-jazzicon/src/components';
 import TokenBalance from 'src/components/common/TokenBalance.vue';
 export default defineComponent({
@@ -82,6 +75,7 @@ export default defineComponent({
     },
   },
   setup(props) {
+    const isExpand = ref<boolean>(false);
     const t = computed<Asset>(() => props.token);
     const { currentNetworkIdx } = useNetworkInfo();
 
@@ -100,9 +94,14 @@ export default defineComponent({
       return currentNetworkIdx.value === endpointKey.ASTAR ? astarBalanceUrl : shidenBalanceUrl;
     });
 
+    const { width, screenSize } = useBreakpoints();
+
     return {
       isDisplayToken,
       explorerLink,
+      isExpand,
+      width,
+      screenSize,
       buildTransferPageLink,
     };
   },
