0aa6b43289d15d011a345180281971902e62bca0
Ayumi Takahashi <ayumee528@gmail.com>
2023-10-25 22:59:49 -0700

    add ads style

--- src/components/assets/Ads.vue
+++ src/components/assets/Ads.vue
@@ -16,6 +16,13 @@
         >Astar zkEVM is Coming</span
       >
     </a>
+    <a class="new-partner" href="#" target="_blank" rel="noopener noreferrer">
+      <span class="new-partner__top">New Partner</span>
+      <span>
+        <span class="new-partner__title">Gridlock</span>
+        <span class="new-partner__subtitle">The innovative wallet</span>
+      </span>
+    </a>
   </div>
 </template>
 <script lang="ts">
@@ -32,6 +39,18 @@ export default defineComponent({
 });
 </script>
 <style lang="scss" scoped>
+.wrapper--ads {
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+  a {
+    box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.2);
+    transition: all 0.2s ease;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+}
 .astar-zkevm {
   display: flex;
   flex-direction: column;
@@ -40,18 +59,39 @@ export default defineComponent({
   font-weight: 800;
   height: 284px;
   padding: 24px;
-  border-radius: 30px;
+  border-radius: 16px;
   line-height: 1.3;
   color: white;
   background: linear-gradient(180deg, #703ac2 26.04%, #226dff 100%);
-  box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.2);
-  transition: all 0.2s ease;
   span {
     display: block;
     width: 180px;
   }
-  &:hover {
-    opacity: 0.8;
+}
+.new-partner {
+  height: 175px;
+  padding: 24px;
+  border-radius: 16px;
+  line-height: 1.3;
+  color: white;
+  background: linear-gradient(180deg, #3e1978 26.04%, #083fa8 100%);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  span {
+    display: block;
+  }
+  .new-partner__top {
+    font-size: 16px;
+    font-weight: 700;
+  }
+  .new-partner__title {
+    font-size: 32px;
+    font-weight: 700;
+  }
+  .new-partner__subtitle {
+    font-size: 14px;
+    font-weight: 700;
   }
 }
 </style>
