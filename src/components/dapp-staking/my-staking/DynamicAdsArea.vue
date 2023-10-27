d47f39026aca664d26040a18e268ca8fec5f3f90
Ayumi Takahashi <ayumee528@gmail.com>
2023-10-27 02:20:11 -0700

    add basic card slide ui

--- src/components/dapp-staking/my-staking/DynamicAdsArea.vue
+++ src/components/dapp-staking/my-staking/DynamicAdsArea.vue
@@ -1,116 +1,144 @@
 <template>
   <div class="wrapper">
-    <div class="wrapper-item news-area">
-      <news-area />
-    </div>
-    <div class="wrapper-item wrapper--banners" :class="isShiden && 'wrapper--shiden'">
-      <div
-        v-for="(t, index) in items"
-        :key="index"
-        class="card"
-        :style="
-          t.gradient
-            ? `background: linear-gradient(180deg, ${t.gradient[0]} 0%, ${t.gradient[1]} 100%)`
-            : ''
-        "
-        @click="goToLink(t.link)"
-      >
-        <div class="wrapper--img">
-          <q-img :src="sourceImg(t.img, index)" class="img--dapp" fit="contain" no-spinner />
-        </div>
-        <div class="card-info">
-          <div class="txt--category">{{ t.category }}</div>
-          <div class="txt--title">
-            {{
-              index === 0 ? $n(truncate(rewardsDeveloper, 0)) + ' ' + nativeTokenSymbol : t.title
-            }}
+    <swiper
+      :slides-per-view="1.25"
+      :slides-per-group="1"
+      :space-between="24"
+      :navigation="true"
+      :parallax="true"
+      :modules="modules"
+      :breakpoints="{
+        '768': {
+          slidesPerView: 3.25,
+          slidesPerGroup: 3,
+        },
+        '1280': {
+          slidesPerView: 4.5,
+          slidesPerGroup: 4,
+        },
+      }"
+    >
+      <swiper-slide v-for="(item, index) in dappPromotions" :key="index">
+        <div class="card" @click="goToLink(item.link)">
+          <img :src="item.img" class="card__img" />
+          <div class="card__bottom">
+            <div>
+              <div class="text--accent">FEATURED</div>
+              <div class="text--title">{{ item.name }}</div>
+            </div>
+            <div class="text--description">{{ item.description }}</div>
           </div>
-          <div class="txt--subtitle">{{ t.subtitle }}</div>
         </div>
-      </div>
-    </div>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent, ref, watchEffect, computed } from 'vue';
-import { Struct } from '@polkadot/types';
-import { Perbill } from '@polkadot/types/interfaces';
-import { ethers } from 'ethers';
-import { $api } from 'src/boot/api';
-import { truncate } from '@astar-network/astar-sdk-core';
-import { endpointKey } from 'src/config/chainEndpoints';
-import { useNetworkInfo } from 'src/hooks';
-import NewsArea from './components/NewsArea.vue';
-import featuredData from 'src/data/featured_dapp.json';
+import dappPromotions from 'src/data/dapp_promotions.json';
+import { networkParam, Path } from 'src/router/routes';
+import { useRouter } from 'vue-router';
 
-interface RewardDistributionConfig extends Struct {
-  readonly baseTreasuryPercent: Perbill;
-  readonly baseStakerPercent: Perbill;
-  readonly dappsPercent: Perbill;
-  readonly collatorsPercent: Perbill;
-  readonly adjustablePercent: Perbill;
-  readonly idealDappsStakingTvl: Perbill;
+// Import Swiper
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+
+interface Data {
+  name: string;
+  description: string;
+  img: string;
+  link?: string;
+  address?: string;
 }
 
 export default defineComponent({
   components: {
-    NewsArea,
+    Swiper,
+    SwiperSlide,
   },
   setup() {
-    const items = featuredData;
-    const rewardsDeveloper = ref<number>(0);
-    const { nativeTokenSymbol, currentNetworkIdx } = useNetworkInfo();
-    const isShiden = computed(() => currentNetworkIdx.value === endpointKey.SHIDEN);
+    const items = ref<Data[]>([]);
+    const router = useRouter();
 
     const goToLink = (link: string) => {
       window.open(link, '_blank');
     };
 
-    const calcDeveloperRewards = async (): Promise<void> => {
-      try {
-        const api = $api!;
-        const [rewardsAllocation, rewardsAmount, blockPerEra] = await Promise.all([
-          api.query.blockReward.rewardDistributionConfigStorage<RewardDistributionConfig>(),
-          api.consts.blockReward.rewardAmount.toString(),
-          Number(api.consts.dappsStaking.blockPerEra.toString()),
-        ]);
-        const rewardsAmountPerEra = Number(ethers.utils.formatEther(rewardsAmount)) * blockPerEra;
-        const numAdjToPercentage = 0.000000001;
-        const dappsPercent = rewardsAllocation.dappsPercent.toNumber() * numAdjToPercentage;
-        rewardsDeveloper.value = rewardsAmountPerEra * dappsPercent;
-      } catch (error) {
-        console.error(error);
-      }
+    const goDappPageLink = (address: string | undefined): void => {
+      const base = networkParam + Path.DappStaking + Path.Dapp;
+      const url = `${base}?dapp=${address?.toLowerCase()}`;
+      router.push(url);
     };
 
-    const sourceImg = (img: string, index: number) => {
-      if (index === 0) {
-        if (isShiden.value) {
-          return require('/src/assets/img/ic_sdn_farm.svg');
-        } else {
-          return require('/src/assets/img/ic_astar_farm.svg');
-        }
-      } else if (index === 1) {
-        return require('/src/assets/img/ic_algem_staking.svg');
-      } else {
-        return img;
-      }
-    };
-
-    watchEffect(calcDeveloperRewards);
+    // dummy data
+    const newListing = [
+      {
+        address: '0x95f506e72777efcb3c54878bb4160b00cd11cd84',
+        name: 'Lucky',
+        description:
+          'New listing1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rutrum.',
+        img: '/images/dapp_promotions/new.webp',
+      },
+      {
+        address: '0x95f506e72777efcb3c54878bb4160b00cd11cd84',
+        name: 'Lucky',
+        description:
+          'New listing1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rutrum.',
+        img: '/images/dapp_promotions/new.webp',
+      },
+    ];
 
     return {
-      nativeTokenSymbol,
-      rewardsDeveloper,
-      truncate,
-      sourceImg,
-      items,
-      isShiden,
+      modules: [Navigation],
+      dappPromotions,
       goToLink,
+      goDappPageLink,
     };
   },
 });
 </script>
 <style lang="scss" scoped>
-@import './styles/dynamic-ads-area.scss';
+// @import './styles/dynamic-ads-area.scss';
+.swiper {
+  overflow: visible;
+}
+.swiper-slide {
+  border-radius: 6px;
+  overflow: hidden;
+  height: auto;
+}
+.card {
+  background: rgba(255, 255, 255, 0.1);
+  height: 100%;
+  transition: all 0.2s ease;
+  cursor: pointer;
+  &:hover {
+    background: rgba(255, 255, 255, 0.15);
+  }
+}
+.card__img {
+  aspect-ratio: 16/9;
+  object-fit: cover;
+  width: 100%;
+}
+.card__bottom {
+  padding: 16px 24px;
+}
+.text--accent {
+  font-size: 12px;
+  font-weight: 700;
+  color: $astar-blue-dark;
+}
+.text--title {
+  margin: 4px 0 16px 0;
+}
+.text--description {
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
+  color: $gray-1;
+}
 </style>
