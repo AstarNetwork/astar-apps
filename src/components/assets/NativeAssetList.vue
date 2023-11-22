669c48587efc0d3ef915de0d8a40b1e7e3b036d6
Ayumi Takahashi <ayumee528@gmail.com>
2023-11-22 15:15:19 +0900

    Revert "Remove transferable and comment out e2e test"
    This reverts commit 787a3bd8ca0ec050a86b783c2137c4b2b980f285.

--- src/components/assets/NativeAssetList.vue
+++ src/components/assets/NativeAssetList.vue
@@ -161,7 +161,7 @@
         <div class="expand-container">
           <div :id="isExpand ? 'asset-expand' : 'asset-expand-close'" class="rows--expand">
             <!-- Transferable -->
-            <!-- <div class="row--expand">
+            <div class="row--expand">
               <div class="row--expand__info">
                 <div class="column--label text--label">{{ $t('assets.transferable') }}</div>
                 <div class="column--balance">
@@ -191,7 +191,7 @@
                   </button>
                 </router-link>
               </div>
-            </div> -->
+            </div>
 
             <!-- Vesting -->
             <div class="row--expand">
