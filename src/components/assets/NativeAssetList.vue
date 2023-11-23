04d96fae59d837146a2c06e54d7a7288aa95689b
impelcrypto <92044428+impelcrypto@users.noreply.github.com>
2023-11-23 19:56:30 +0900

    fix: include vesting amount in locked tokens (#1043)

--- src/components/assets/NativeAssetList.vue
+++ src/components/assets/NativeAssetList.vue
@@ -136,7 +136,7 @@
             </div>
             <div v-if="!isSkeleton" class="column--balance">
               <div class="column--amount text--amount">
-                {{ isTruncate ? $n(truncate(lockInDappStaking, 3)) : Number(lockInDappStaking) }}
+                {{ $n(truncate(lockInDappStaking + vestingTtl, 3)) }}
               </div>
               <div class="column--symbol text--symbol">
                 {{ nativeTokenSymbol }}
