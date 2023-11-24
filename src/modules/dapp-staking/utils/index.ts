6836cda5d1bb7bceb518e54db258159487147a76
impelcrypto <impelcrypto@gmail.com>
2023-11-24 17:02:01 +0900

    fix: display dapp data

--- src/modules/dapp-staking/utils/index.ts
+++ src/modules/dapp-staking/utils/index.ts
@@ -1,3 +1,4 @@
+import { ApiPromise } from '@polkadot/api';
 import { LOCAL_STORAGE } from 'src/config/localStorage';
 
 export const checkIsLimitedProvider = (): boolean => {
@@ -7,4 +8,8 @@ export const checkIsLimitedProvider = (): boolean => {
   );
   const endpoint = String(Object.values(selectedEndpoint)[0]);
   return limitedProvider.some((it) => endpoint.includes(it));
+};
+
+export const checkIsDappStakingV3 = (api: ApiPromise) => {
+  return Object.prototype.hasOwnProperty.call(api.query, 'dappStaking');
 };
