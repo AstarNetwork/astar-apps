8f20945cbe8f493051880feadb25fac9ff11b15a
impelcrypto <impelcrypto@gmail.com>
2023-11-29 12:59:08 +0900

    feat: added owner page

---
+++ src/staking-v3/components/Owner.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="wrapper--owner">
+    <span>This is Owner page</span>
+    <div v-if="dappAddress">
+      <p>{{ dappAddress }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+export default defineComponent({
+  components: {},
+  setup() {
+    const route = useRoute();
+    const dappAddress = computed<string>(() => route.query.dapp as string);
+    return { dappAddress };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@use './styles/owner.scss';
+</style>
