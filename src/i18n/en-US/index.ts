002a8103fd55b7235c84d6bcc93a1b58ec4ee5ed
Ayumi Takahashi <ayumee528@gmail.com>
2024-01-03 22:54:29 -0800

    hotfix: update claim warning banner text (#1110)

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -68,7 +68,7 @@ export default {
     withdrawalNotSupport: "The portal doesn't support withdrawing to {chain} at this moment",
     ledgerNotOpened: 'Ledger has not opened Astar App. Please open it and refresh this page.',
     claimRewards:
-      'DApp Staking V3 is coming early next year! Make sure to claim your rewards and stay tuned for the release date.',
+      'DApp Staking V3 is coming early 2024! Make sure to claim your rewards and stay tuned for the release date.',
   },
   toast: {
     transactionFailed: 'Transaction failed with error: {message}',
