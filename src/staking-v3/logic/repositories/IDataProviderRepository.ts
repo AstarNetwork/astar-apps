2c4ec83f378a7219b340889c14bba598f28d4c81
Bobo <bobo.kovacevic@gmail.com>
2024-01-11 12:45:55 +0100

    Display number of stakers (#1121)
    * Fetch and display number of stakers
    
    * Mutations refactoring
    
    * Cleanup

---
+++ src/staking-v3/logic/repositories/IDataProviderRepository.ts
@@ -0,0 +1,8 @@
+import { ProviderDappData } from '../models';
+
+/**
+ * Interface for provider to fetch dapp staking v3 data from other sources
+ */
+export interface IDataProviderRepository {
+  getDapps(network: string): Promise<ProviderDappData[]>;
+}
