c9e4b3017102d012b90dbf864db0830a662022b8
Gregory Luneau <greg@astar.network>
2024-01-12 11:29:24 -0500

    fix: toast for `You do not have enough tokens to pay the transaction fee` (#1116)
    * Minimum balance on the network is 0.01
    
    * draft for evm
    
    * 0.05 minimum and custom message
    
    * evm message
    
    * REQUIRED_MINIMUM_BALANCE
    
    * message change
    
    * standardize message
    
    * migrate check downstream to substract amount before check
    
    * positive spin and decimal const
    
    * formatEther
    
    * PolkadotWalletService minimum balance check

--- src/v2/services/implementations/PolkadotWalletService.ts
+++ src/v2/services/implementations/PolkadotWalletService.ts
@@ -8,7 +8,7 @@ import { inject, injectable } from 'inversify';
 import { LOCAL_STORAGE } from 'src/config/localStorage';
 import { isMobileDevice } from 'src/hooks/helper/wallet';
 import { getSubscanExtrinsic, polkasafeUrl } from 'src/links';
-import { AlertMsg } from 'src/modules/toast/index';
+import { AlertMsg, REQUIRED_MINIMUM_BALANCE } from 'src/modules/toast/index';
 import { Guard, wait } from 'src/v2/common';
 import { BusyMessage, ExtrinsicStatusMessage, IEventAggregator } from 'src/v2/messaging';
 import { Account } from 'src/v2/models';
@@ -69,6 +69,16 @@ export class PolkadotWalletService extends WalletService implements IWalletServi
     try {
       return new Promise<string>(async (resolve, reject) => {
         isDetectExtensionsAction && this.detectExtensionsAction(true);
+
+        const useableBalance = await this.assetsRepository.getNativeBalance(senderAddress);
+        const isBalanceEnough =
+          Number(ethers.utils.formatEther(useableBalance)) > REQUIRED_MINIMUM_BALANCE;
+        if (!isBalanceEnough) {
+          this.eventAggregator.publish(
+            new ExtrinsicStatusMessage({ success: false, message: AlertMsg.MINIMUM_BALANCE })
+          );
+          throw new Error(AlertMsg.MINIMUM_BALANCE);
+        }
 
         await this.checkExtension();
         let tip = transactionTip?.toString();
