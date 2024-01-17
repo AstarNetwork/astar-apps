e9de457ee86e33d7c46ee1383789c38bf522f960
impelcrypto <92044428+impelcrypto@users.noreply.github.com>
2024-01-17 22:44:35 +0800

    hotfix: added a vesting note (#1141)
    * hotfix: added a vesting note
    
    * fix: condition of displaying the note

--- src/components/assets/NativeAssetList.vue
+++ src/components/assets/NativeAssetList.vue
@@ -166,6 +166,21 @@
                     <div class="column--symbol text--symbol">
                       {{ nativeTokenSymbol }}
                     </div>
+                    <div v-if="vestingTtl > 0" class="row--vesting-note">
+                      <span>
+                        {{
+                          $t('assets.vestingInStake', {
+                            amount: $n(
+                              truncate(
+                                vestingTtl > lockInDappStaking ? lockInDappStaking : vestingTtl,
+                                3
+                              )
+                            ),
+                            token: nativeTokenSymbol,
+                          })
+                        }}
+                      </span>
+                    </div>
                   </template>
                   <template v-else>
                     <div class="skeleton--right">
