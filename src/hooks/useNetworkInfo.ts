09c35b880057d5c2a21132dac2e12f01eca85c90
impelcrypto <impelcrypto@gmail.com>
2024-03-06 17:22:33 +0800

    fix: clean up

--- src/hooks/useNetworkInfo.ts
+++ src/hooks/useNetworkInfo.ts
@@ -76,7 +76,7 @@ export function useNetworkInfo() {
   });
 
   const dappStakingCurrency = computed<string>(() => {
-    // Memo: avoid displaying ETH'
+    // Memo: avoid displaying 'ETH'
     if (isZkEvm.value) {
       return isAstarZkEvm.value ? 'ASTAR' : 'SBY';
     } else {
