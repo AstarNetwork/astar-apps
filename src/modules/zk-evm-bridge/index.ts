8afef4b960ef79d2498abd48e91f2421ae03ca54
impelcrypto <impelcrypto@gmail.com>
2024-03-14 22:45:57 +0800

    feat: updated layerswap url

--- src/modules/zk-evm-bridge/index.ts
+++ src/modules/zk-evm-bridge/index.ts
@@ -3,4 +3,5 @@ export * from './tokens';
 
 export const stargateUrl = 'https://stargate.finance/transfer';
 export const relayBridgeAppLink = 'https://relay.link/bridge/astar-zkevm/?fromChainId=1';
-export const layerSwapLink = 'https://www.layerswap.io/app';
+export const layerSwapLink =
+  'https://www.layerswap.io/app?to=astarzk_mainnet&from=ethereum_mainnet';
