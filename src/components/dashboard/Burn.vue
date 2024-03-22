f3f6a369c31fd9289598ed59fc1463bcb76ad295
Taegeon Go (Alan) <tae.gun7784@gmail.com>
2024-03-22 18:47:45 +0900

    Add Burn component to dashboard

---
+++ src/components/dashboard/Burn.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrapper--value" data-testid="burn-status">
+    <div class="container container--value">
+      <div class="row--title">
+        <span class="text--accent container--title--color"> Burn </span>
+        <span class="text--accent container--title--color"> Coming Soon </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {},
+});
+</script>
+
+<style lang="scss" scoped>
+@use 'src/components/dashboard/styles/burn.scss';
+</style>
