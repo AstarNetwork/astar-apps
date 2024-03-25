4817d9d2012c63908b0aa5a12cd6b8407e397cbe
impelcrypto <impelcrypto@gmail.com>
2024-03-25 22:27:04 +0800

    fix: price fetching

--- src/v2/repositories/implementations/TokenApiRepository.ts
+++ src/v2/repositories/implementations/TokenApiRepository.ts
@@ -7,9 +7,12 @@ export class TokenApiRepository implements IPriceRepository {
   public static BaseUrl = 'https://api.astar.network/api';
 
   public async getUsdPrice(symbol: string): Promise<number> {
-    const url = `${TokenApiRepository.BaseUrl}/v1/token/price/${symbol}`;
-    const response = await axios.get<number>(url);
-
-    return response.data;
+    try {
+      const url = `${TokenApiRepository.BaseUrl}/v1/token/price/${symbol}`;
+      const response = await axios.get<number>(url);
+      return response.data;
+    } catch (error) {
+      return 0;
+    }
   }
 }
