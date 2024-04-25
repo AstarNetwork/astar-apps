18072b6a1d505473c0ef18ef1ca3a886a5dc48a5
impelcrypto <impelcrypto@gmail.com>
2024-04-25 11:45:33 +0800

    fix: clean up

--- src/v2/services/implementations/MetamaskWalletService.ts
+++ src/v2/services/implementations/MetamaskWalletService.ts
@@ -123,6 +123,13 @@ export class MetamaskWalletService extends WalletService implements IWalletServi
           }
         }
 
+        // Todo: remove this logic once contract address for Shiden lockdrop has been updated to align with Astar and Shibuya
+        const chainId = await web3.eth.getChainId();
+        const isShiden = chainId === 336;
+        const contractAddress = isShiden
+          ? '0x0000000000000000000000000000000000005005'
+          : evmPrecompiledContract.lockdropDispatch;
+
         const hexEncodedCall = extrinsic.method.toHex();
         const msg = 'Signing transaction for hex-encoded call: ' + hexEncodedCall;
         const signature = (await this.provider.request({
@@ -130,14 +137,6 @@ export class MetamaskWalletService extends WalletService implements IWalletServi
           params: [h160Address, msg],
         })) as string;
         const { uncompressedPubKey } = utils.recoverPublicKeyFromSig(h160Address, msg, signature);
-
-        // Todo: remove this logic once contract address for Shiden lockdrop has been updated to align with Astar and Shibuya
-        const chainId = await web3.eth.getChainId();
-        const isShiden = chainId === 336;
-
-        const contractAddress = isShiden
-          ? '0x0000000000000000000000000000000000005005'
-          : evmPrecompiledContract.lockdropDispatch;
 
         const contract = new web3.eth.Contract(lockdropDispatchAbi as AbiItem[], contractAddress);
 
