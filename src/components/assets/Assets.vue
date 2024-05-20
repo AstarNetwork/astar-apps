db3a394a52b66fb7d094be1e3c151715fe62176c
Bobo <bobo.kovacevic@gmail.com>
2024-05-20 11:20:35 +0200

    Restored side ads

--- src/components/assets/Assets.vue
+++ src/components/assets/Assets.vue
@@ -81,6 +81,7 @@ import AnchorLinks from 'src/components/assets/AnchorLinks.vue';
 import EvmAssetList from 'src/components/assets/EvmAssetList.vue';
 import EvmNativeToken from 'src/components/assets/EvmNativeToken.vue';
 import NativeAssetList from 'src/components/assets/NativeAssetList.vue';
+import SideAds from 'src/components/assets/SideAds.vue';
 import XcmNativeAssetList from 'src/components/assets/XcmNativeAssetList.vue';
 import YourProject from 'src/components/assets/YourProject.vue';
 import ZkAstr from 'src/components/assets/ZkAstr.vue';
@@ -101,6 +102,7 @@ import { computed, defineComponent, onUnmounted, ref, watch, watchEffect } from 
 export default defineComponent({
   components: {
     Account,
+    SideAds,
     AstarDomains,
     EvmAssetList,
     XcmNativeAssetList,
