864905d3b0de143b9fcb1eb3d5be9d0562d29b4d
Bobo <bobo.kovacevic@gmail.com>
2024-05-31 09:27:38 +0200

    Remove Yoki campaign (#1297)

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -46,14 +46,6 @@ export default {
   help: 'Help',
   share: 'Share',
   multisig: 'Multisig',
-  modals: {
-    yokiOrigins: {
-      introducing: 'Introducing a new experience on zkEVM',
-      experienceNow: 'Experience now',
-      learnMore: 'Learn more',
-      keep: 'Keep me on the Portal',
-    },
-  },
   sort: {
     sortBy: 'Sort by',
     amountHightToLow: 'Amount: High to Low',
@@ -86,8 +78,6 @@ export default {
     ledgerUsers: 'Ledger Users',
     ledgerUsersImportantInformation:
       'Important information regarding the upcoming transition to dApp Staking V3. Check out the details',
-    yoki: 'Yoki Origins',
-    yokiSignUp: 'Yoki Origins is a user onboarding journey on Astar zkEVM. Experience now!',
     stakingNotSupportZkEvm:
       'dApps Staking is not supported on {networkNotSupport}, please switch to {networkSupport} and start staking!',
     connectedInvalidNetwork: 'Please connect to the correct network in your wallet',
