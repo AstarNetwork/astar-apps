aeb517e1d39f77bcc4b91214a3c0116aa829b404
Bobo <bobo.kovacevic@gmail.com>
2024-07-29 13:22:22 +0200

    Register dapp test fixes

--- tests/test_specs/assets.spec.ts
+++ tests/test_specs/assets.spec.ts
@@ -1,6 +1,5 @@
 import { clickDisclaimerButton } from 'src/modules/playwright';
 import { expect } from '@playwright/test';
-import { ApiPromise } from '@polkadot/api';
 import {
   ALICE_ACCOUNT_NAME,
   ALICE_ACCOUNT_SEED,
@@ -12,18 +11,7 @@ import {
   selectAccount,
   selectMultisigAccount,
 } from '../common';
-import { getApi } from '../common-api';
 import { test } from '../fixtures';
-import { wait } from '@astar-network/astar-sdk-core';
-
-let api: ApiPromise;
-test.beforeAll(async () => {
-  api = await getApi();
-});
-
-test.afterAll(async () => {
-  await api.disconnect();
-});
 
 test.beforeEach(async ({ page, context }) => {
   // TODO consider moving this into beforeAll
--- tests/test_specs/dappstaking-register.spec.ts
+++ tests/test_specs/dappstaking-register.spec.ts
@@ -9,18 +9,6 @@ import {
   createAccount,
   selectAccount,
 } from '../common';
-import { ApiPromise } from '@polkadot/api';
-import { getApi } from '../common-api';
-import { wait } from '@astar-network/astar-sdk-core';
-
-let api: ApiPromise;
-test.beforeAll(async () => {
-  api = await getApi();
-});
-
-test.afterAll(async () => {
-  await api.disconnect();
-});
 
 test.beforeEach(async ({ page, context }: { page: Page; context: BrowserContext }) => {
   await page.goto('/astar/dapp-staking/discover');
@@ -34,7 +22,7 @@ test.beforeEach(async ({ page, context }: { page: Page; context: BrowserContext 
 
   await closePolkadotWelcomePopup(context);
   await createAccount(page, BOB_ACCOUNT_SEED, BOB_ACCOUNT_NAME);
-  await page.goto('/astar/dapp-staking/discover');
+  await page.goto('/astar/assets');
   await connectToNetwork(page);
   await page.waitForSelector('.four', { state: 'hidden' }); // Wait for the page to load
   await selectAccount(page, BOB_ACCOUNT_NAME);
@@ -42,11 +30,7 @@ test.beforeEach(async ({ page, context }: { page: Page; context: BrowserContext 
 
 test.describe('register dApp page', () => {
   test('validate builder image size', async ({ page }) => {
-    await page
-      .getByRole('link', {
-        name: '🎉 Congrats!! your contract is approved. Please submit the details Register now',
-      })
-      .click();
+    await page.getByTestId('register-dapp-banner').click();
     await page
       .locator('.builders--container > .card > .card--image > .slot > .card-new-item')
       .click();
