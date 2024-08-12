3eea96f5ca1f8f7a3199cf1de9bd9e26e2d386cf
Bobo <bobo.kovacevic@gmail.com>
2024-08-12 14:45:54 +0200

    Enable users to update metadata once more event if it is up to date

--- src/config/localStorage.ts
+++ src/config/localStorage.ts
@@ -22,6 +22,7 @@ export enum LOCAL_STORAGE {
   MULTISIG = 'multisig',
   CLOSE_DAPP_STAKING_V3_ONBOARDING = 'closeDappStakingV3Onboarding',
   DECOMMISSION = 'decommission',
+  HAS_RAW_METADATA_V15 = 'hasRawMetadataV15',
 }
 
 // Memo: A helper function to return the account's history data that is stored in the browser
