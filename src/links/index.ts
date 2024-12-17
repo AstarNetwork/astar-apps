f37807884beb0097d7dc7f35e3bfbf490491816e
codespool <4625220+codespool@users.noreply.github.com>
2024-12-18 00:48:42 +0200

    update link

--- src/links/index.ts
+++ src/links/index.ts
@@ -1,6 +1,6 @@
 import { endpointKey, providerEndpoints } from 'src/config/chainEndpoints';
 import { blockExplorerUrls } from 'src/config/web3';
-import Web3 from 'web3';
+import type Web3 from 'web3';
 
 export const docsUrl = {
   topPage: 'https://docs.astar.network',
@@ -53,7 +53,7 @@ export const polkadotJsUrl = {
   },
 };
 
-export const lfgmUrl = 'https://lfgm.astar.network';
+export const surgeUrl = 'https://surge.sakefinance.com/';
 
 export const ccipExplorerUrl = 'https://ccip.chain.link';
 
